b7b5ee54dfabd7c7d7377d89d5cb75901df9b8b3
Christoph Schürz <christoph.schuerz@ufz.de>
2022-10-10 13:11:19 +0200

    Initiate utils.R and add get_os() function
    - Initiated R script where utility functions can be collected
    - added a get operation system function in utils.R

---
+++ R/utils.R
@@ -0,0 +1,28 @@
+# utils.R
+# Utility functions which are used in different R functions
+
+#' Identify the operating system.
+#' The function was written by Will Lowe and was copied from here:
+#' https://conjugateprior.org/2015/06/identifying-the-os-from-r/
+#'
+#' @keywords internal
+#'
+get_os <- function(){
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)){
+    os <- sysinf['sysname']
+    if (os == 'Darwin') {
+      os <- "osx"
+    }
+  # If rare case occurs that Sys.info() is NULL
+  } else {
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os)) {
+      os <- "osx"
+    }
+    if (grepl("linux-gnu", R.version$os)) {
+      os <- "linux"
+    }
+  }
+  tolower(os)
+}
