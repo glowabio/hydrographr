752325a74c8bb99274ab6104e6dc7ae6b2b2c6cd
Christoph Schürz <christoph.schuerz@boku.ac.at>
2022-10-20 12:03:06 +0200

    add inst folder for Linux and Windows sh/bat scripts Init .bat/.sh script for reclass raster

---
+++ inst/win/bat/reclass.bat
@@ -0,0 +1,5 @@
+@ECHO ON
+
+wsl dos2unix /mnt/c/Users/maria/Documents/test/reclass_20_1300009.txt
+wsl dos2unix /mnt/c/Users/maria/Documents/test/reclass.sh 
+wsl bash  /mnt/c/Users/maria/Documents/test/reclass.sh
