de45c08f615afdc653fcff6de057c9951ca1f896
Christoph Schürz <christoph.schuerz@boku.ac.at>
2022-10-20 18:11:51 +0200

    Changed folder structure back

--- inst/win/bat/reclass.bat
+++
@@ -1,5 +0,0 @@
-@ECHO ON
-
-wsl dos2unix /mnt/c/Users/maria/Documents/test/reclass_20_1300009.txt
-wsl dos2unix /mnt/c/Users/maria/Documents/test/reclass.sh 
-wsl bash  /mnt/c/Users/maria/Documents/test/reclass.sh
