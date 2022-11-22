b9dc7afba20707f66ab716f6e3027046f1c10e73
afrogri37 <afroditi.grigoropoulou@igb-berlin.de>
2022-11-22 17:01:28 +0100

    runs bash script only once, changed stdout to import a tmp file

--- R/extract_ids.R
+++ R/extract_ids.R
@@ -1,84 +1,45 @@
-#' Join sub-catchment IDs with occurrence points
+#' Adds sub-catchment and/or basin IDs to a dataframe of points
 #'
-#' Add the column 'subcID' to the dataset in 'dataset_path' and export the
-#' data frame in a csv.
-#' In the current version the long-lat values must be in the 2nd and 3rd
-#' column of the data set text file.
-#'
-#' @param dataset_path Path to data set text file with the lat long columns
+#' @param data data.frame with the lat lon columns
 #' @param lon Name of the longitude column
 #' @param lat Name of the latitude column
 #' @param subc_path Path to the sub-catchment ID .tif layer
 #' @param basin_path Path to the basin ID .tif layer
 #' @importFrom data.table fread
 #' @importFrom processx run
-#' @importFrom rlang is_missing
 #' @export
 #'
-extract_ids <- function(dataset_path, lon, lat, subc_path, basin_path) {
+#'
+extract_ids <- function(data, lon, lat, subcatchment_path = NULL, basin_path = NULL) {
+  # Remove all files in the tmp folder
+  unlink(paste0(tempdir(), "/*"))
 
-  # Import taxon occurrence points
-  dataset <- fread(dataset_path, header = TRUE, keepLeadingZeros = TRUE)
+  # Export taxon occurrence points
+  dataset_tmp_path <- paste0(tempdir(), "/points_dataset.txt")
+  fwrite(data, dataset_tmp_path, col.names = TRUE,
+         row.names = FALSE, quote = FALSE, sep = " ")
 
-  # To extract only subcatchment ids
-  if (missing(basin_path) || is.na(basin_path)) {
+  # Convert null arguments to 0 so that bash can evaluate the variables
+  subc_path <- ifelse(is.null(subcatchment_path), 0, subcatchment_path)
+  bas_path <- ifelse(is.null(basin_path), 0, basin_path)
 
   # Call the external .sh script extract_ids() containing the gdal function
   subc_ids <- run(system.file("sh", "extract_ids.sh",
                               package = "hydrographr"),
-                           args = c(dataset_path, lon, lat, subc_path),
-                           echo = FALSE)$stdout
-  # Format the ids string
-  subc_ids <- data.frame(subc_id = as.integer(
-    strsplit(subc_ids, "\n", "", fixed = TRUE)[[1]]
-    ))
+                  args = c(dataset_tmp_path, lon, lat, subc_path,
+                            bas_path, tempdir()),
+                  echo = FALSE)
 
-  # Join the IDs with the observations
-  data_ids <- cbind.data.frame(dataset, subc_ids)
+  # Read in the file containing the ids
+  data_ids <- fread(paste0(tempdir(), "/ids.txt"), keepLeadingZeros = TRUE,
+                    header = T, sep = " ")
 
-  # To extract only basin ids
-} else if (missing(subc_path) || is.na(subc_path)) {
+  # Remove all files in the tmp folder
+  unlink(paste0(tempdir(), "/*"))
 
-  basin_ids <- run(system.file("sh", "extract_ids.sh",
-                               package = "hydrographr"),
-                       args = c(dataset_path, lon, lat, basin_path),
-                       echo = FALSE)$stdout
-  # Format the ids string
-  basin_ids <- data.frame(basin_ids = as.integer(
-    strsplit(basin_ids, "\n", "", fixed = TRUE)[[1]]
-  ))
-
-  # Join the IDs with the observations
-  data_ids <- cbind.data.frame(dataset, basin_ids)
-
-   # To extract both subcatchment and basin ids
-} else {
-  # subcatchment
-  subc_ids <- run(system.file("sh", "extract_ids.sh",
-                              package = "hydrographr"),
-                            args = c(dataset_path, lon, lat, subc_path),
-                            echo = FALSE)$stdout
-
-  # Format the ids string
-  subc_ids <- data.frame(subc_id = as.integer(
-    strsplit(subc_ids, "\n", "", fixed = TRUE)[[1]]
-  ))
-  # basin
-  basin_ids <- run(system.file("sh", "extract_ids.sh",
-                               package = "hydrographr"),
-                             args = c(dataset_path, lon, lat, basin_path),
-                             echo = FALSE)$stdout
-  # Format the ids string
-  basin_ids <- data.frame(basin_ids = as.integer(
-    strsplit(basin_ids, "\n", "", fixed = TRUE)[[1]]
-  ))
-
-  # Join the IDs with the observations
-  data_ids <- cbind.data.frame(dataset, subc_ids, basin_ids)
-}
-
+  # Return data frame
   return(data_ids)
 
 
-  }
+}
 
--- inst/sh/extract_ids.sh
+++ inst/sh/extract_ids.sh
@@ -3,18 +3,60 @@
 export DATA=$1
 export LON=$2
 export LAT=$3
-export LAYER=$4
+export SUBC_LAYER=$4
+export BASIN_LAYER=$5
+export TMPDIR=$6
 
-# The $LAYER is a path either to the subcatchment or to the basin id layer
+if [ "$SUBC_LAYER" = "0"  ] ; then
 
-awk -v LON=$LON -v LAT=$LAT '
-NR==1 {
-    for (i=1; i<=NF; i++) {
-        f[$i] = i
+    # add header
+    echo "basin_id" > $TMPDIR/ids.txt
+
+    awk -v LON=$LON -v LAT=$LAT '
+    NR==1 {
+        for (i=1; i<=NF; i++) {
+            f[$i] = i
+        }
     }
-}
-{ if(NR>1) {print $(f[LON]), $(f[LAT]) }}
-' $DATA   | gdallocationinfo -valonly -geoloc  $LAYER
+    { if(NR>1) {print $(f[LON]), $(f[LAT]) }}
+    ' $DATA   | gdallocationinfo -valonly -geoloc  $BASIN_LAYER >> $TMPDIR/ids.txt
 
+elif [ "$BASIN_LAYER" = "0"  ] ; then
 
+    # add header
+    echo "subcatchment_id" > $TMPDIR/ids.txt
+
+    awk -v LON=$LON -v LAT=$LAT '
+    NR==1 {
+        for (i=1; i<=NF; i++) {
+            f[$i] = i
+        }
+    }
+    { if(NR>1) {print $(f[LON]), $(f[LAT]) }}
+    ' $DATA   | gdallocationinfo -valonly -geoloc  $SUBC_LAYER >> $TMPDIR/ids.txt
+
+else
+   echo "subcatchment_id" > $TMPDIR/subc_ids.txt
+   awk -v LON=$LON -v LAT=$LAT '
+        NR==1 {
+            for (i=1; i<=NF; i++) {
+                f[$i] = i
+            }
+        }
+        { if(NR>1) {print $(f[LON]), $(f[LAT]) }}
+        ' $DATA   | gdallocationinfo -valonly -geoloc  $SUBC_LAYER >> $TMPDIR/subc_ids.txt
+
+    echo "basin_id" > $TMPDIR/basin_ids.txt
+    awk -v LON=$LON -v LAT=$LAT '
+        NR==1 {
+            for (i=1; i<=NF; i++) {
+                f[$i] = i
+             }
+        }
+        { if(NR>1) {print $(f[LON]), $(f[LAT]) }}
+        ' $DATA   | gdallocationinfo -valonly -geoloc  $BASIN_LAYER >> $TMPDIR/basin_ids.txt
+
+    paste -d" " $DATA $TMPDIR/subc_ids.txt $TMPDIR/basin_ids.txt > $TMPDIR/ids.txt
+
+ fi
 
