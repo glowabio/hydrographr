fdc167d7f5e095808d8f565890d5d58f982d24f9
domisch <domisch@igb-berlin.de>
2022-11-22 12:32:14 +0100

    Cleaned params

--- R/table_to_graph.R
+++ R/table_to_graph.R
@@ -1,10 +1,9 @@
 
 #' Convert a table to a graph
 #'
-#' Convert a table as an directed or undirected graph (igraph object). The attributes will be attached to the edges (i.e. the stream segments), and the input table nees to have the "stream" and "next_stream" as the first two columns.
+#' Convert a table as an directed or undirected graph (igraph object), where the attributes will be attached to the edges (i.e. the stream segments). The input table needs to have the "stream" and "next_stream" (or any other name) as the first two columns.
 #'
-#' @param filename A data.frame or data.table that has the "stream" as the 1st column, and "next_stream" as the 2nd column.
-#'
+#' @param filename A data.frame or data.table that has the "stream" as the 1st column, and "next_stream" as the 2nd column. Column names are flexible.
 #' @param directed If TRUE (the default), then the output will be a directed graph, else an undirected graph.
 
 #' @importFrom data.table setDT
@@ -14,16 +13,14 @@
 
 
 
-
-
-usePackage <- function(p){
-  if (!is.element(p, installed.packages()[,1])) install.packages(p, dep = TRUE)
-  library(p, character.only = TRUE)
-}
-
-usePackage("DBI")
-usePackage("data.table")
-usePackage("igraph")
+# usePackage <- function(p){
+#   if (!is.element(p, installed.packages()[,1])) install.packages(p, dep = TRUE)
+#   library(p, character.only = TRUE)
+# }
+#
+# usePackage("DBI")
+# usePackage("data.table")
+# usePackage("igraph")
 
 
 
@@ -44,6 +41,6 @@ table_to_graph <- function(filename, directed=T) {
 
 
 # test function
-my_graph <- table_to_graph(my_table)
+# my_graph <- table_to_graph(my_table, directed=T)
 
 
