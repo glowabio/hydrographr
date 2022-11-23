09cca1c5c80948ad32f424d643ca5b11b2f111e1
domisch <domisch@igb-berlin.de>
2022-11-23 16:25:13 +0100

    Fixed input types and added error messages

--- R/read_geopackage.R
+++ R/read_geopackage.R
@@ -5,7 +5,7 @@
 #'
 #' @param filename Name of the GeoPackage file to import, e.g. "order_vect_59.gpkg"
 #' @param dt If TRUE, import the GeoPackage as a data.table
-#' @param graph If TRUE, import the GeoPackage as a directed graph (igraph object)
+#' @param g If TRUE, import the GeoPackage as a directed graph (igraph object)
 #' @param sf If TRUE, import the GeoPackage as a spatial dataframe (sf object)
 #' @param SpatVect If TRUE, import the GeoPackage as a SpatVecteor (terra object)
 
@@ -43,46 +43,74 @@
 
 
 # Import a geopackage from disk as a data.table
-read_geopackage <- function(filename, dt=F, graph=F, sf=F, SpatVect=F) {
+read_geopackage <- function(filename, dt=F, g=F, sf=F, SpatVect=F) {
+
+  # Test input argumnets
+  if(missing(filename)) stop("Please specify the input geopackage file.")
+  if(!grepl(".gpkg", filename, fixed = TRUE)) stop("Input must be a GeoPackage file.")
+  # If all options are missing
+  if(missing(dt) & missing(g) & missing(sf) & missing(SpatVect)) {
+    stop("Please select one output type.\n")
+  }
+
   # Avoid exponential numbers in the table and IDs, only set this only within the function
   options(scipen=999)
+
+  if(dt==TRUE || g==TRUE) {
+    if(dt==TRUE & g==F)  cat("Importing as a data.table...\n")
+    if(g==TRUE)  cat("Importing as a graph...\n")
+
   # create a connection to the SQLite database
+  cat("Opening SQL connection...\n")
   conn <- dbConnect(drv=RSQLite::SQLite(), dbname=filename) # get connection
-  conn_table <- dbListTables(conn) # ## list all tables
-  conn_table <- conn_table[1] # "SELECT"
+  # conn_table <- dbListTables(conn) # list all tables
+  conn_table <- "merged" # "SELECT"
+  cat("Reading GeoPackage file...\n")
   network_table <- dbGetQuery(conn=conn, statement=paste0("SELECT * FROM '", conn_table, "'")) # Read the database
   setDT(network_table) # convert to data.table
   #If vertices is NULL, then the first two columns of d are used as a symbolic edge list and additional columns as edge attributes. The names of the attributes are taken from the names of the columns. Remove the columns for the subsequent igraph functions.
-  network_table$geom <- NULL
-  network_table$fid <- NULL
-  network_table$cat <- NULL
+  try(network_table[, c("geom","fid", "cat"):=NULL], silent = T)
+  # network_table$geom <- NULL
+  # network_table$fid <- NULL
+  # network_table$cat <- NULL
+  cat("Closing SQL connection...\n")
   dbDisconnect(conn) # close db connection
 
-  if(dt==T) {
+  if(dt==T & g==F) {
     return(network_table) }
 
-  if (graph==T) {
-    g <- graph_from_data_frame(network_table, directed = T)
-    return(g) }
+  if (g==T) {
+    g_out <- graph_from_data_frame(network_table, directed = T)
+    return(g_out) }
 
-  if(sf==T) {
-    sf <- read_sf("order_vect_59.gpkg")
-    return(sf)  }
-
-  if (SpatVect==T) {
-    sf <- read_sf("order_vect_59.gpkg")
+} else if (sf==T) {
+    cat("Importing as a sf spatial dataframe...\n")
+    sf <- read_sf(filename, as_tibble=F)
+    return(sf)  } else if (SpatVect==T) {
+    cat("Importing as a terra SpatVect file...\n")
+    sf <- read_sf(filename)
     vect <- vect(sf)
-    return(vect)  }
+    return(vect)
+    }
 
 }
 
 
 ### Test function
 my_table <- read_geopackage("order_vect_59.gpkg", dt=T) # loads a data.table
-my_graph <- read_geopackage("order_vect_59.gpkg", graph=T) # loads as an directed graph
-my_sf <- read_geopackage("order_vect_59.gpkg", sf=T) # loads as an directed graph
-my_vect <- read_geopackage("order_vect_59.gpkg", SpatVect=T) # loads as an directed graph
+my_graph <- read_geopackage("order_vect_59.gpkg", g=T) # loads as an directed graph
+my_sf <- read_geopackage("order_vect_59.gpkg", sf=T) # loads as an spatial dataframe
+my_vect <- read_geopackage("order_vect_59.gpkg", SpatVect=T) # loads as an terra object
 
 
+big file:
+filename="order_vect_segment_h10v08.gpkg"
+my_graph <- read_geopackage("order_vect_segment_h10v08.gpkg", g=T)
 
 
+# small file
+filename="order_vect_segment_h00v00.gpkg"
+my_table <- read_geopackage("order_vect_segment_h00v00.gpkg", dt=T)
+my_graph <- read_geopackage("order_vect_segment_h00v00.gpkg", g=T)
+my_sf <- read_geopackage("order_vect_segment_h00v00.gpkg", sf=T)
+my_vect <- read_geopackage("order_vect_segment_h00v00.gpkg", SpatVect=T)
