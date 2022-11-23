64ce71fa07890532c218e9dc7703e66dd2459e67
Thomas Tomiczek <ttomiczek@AD.IGB-BERLIN.DE>
2022-11-23 18:55:22 +0100

    function test
    R script ready to be tested, works in bash

--- R/merge_tile.R
+++ R/merge_tile.R
@@ -1,11 +1,11 @@
 #' Merge raster or spatial vector tiles
 #'
-#' @param tile_path Path to the raster tile or spatial vector
+#' @param tile_path Path to raster tile or spatial vector
 #' @importFrom processx run
 #' @export
 #'
 
-merge_tiles <- function() {
+merge_tiles <- function(tile_path) {
 	 if (missing(tile_path) || is.na(tile_path)) {
    tile_path <- "NOT_ASSIGNED" 
    } else {
@@ -13,5 +13,5 @@ merge_tiles <- function() {
                            package = "hydrographr"),
                    args = c(tile_path),
                    echo = FALSE)$stdout }
- 	 return(merge_tiles) 
+ 	 return(merge_tiles)
  }
