b4b6f55e8310a4b31a627fa4c9688d7c3ef90796
domisch <domisch@igb-berlin.de>
2022-11-24 17:23:48 +0100

    Added sub-catchments and check arguments

--- R/read_geopackage.R
+++ R/read_geopackage.R
@@ -4,11 +4,12 @@
 #' Read a Geopackage vector file from disk either as a table (data.table), as a directed graph object (igraph), a spatial dataframe (sf) or a SpatVect object (terra).
 #'
 #' @param filename Name of the GeoPackage file to import, e.g. "order_vect_segment_h00v00.gpkg"
+#' @param type Either "net" for importing a network, or "catch" for sub-catchments.
 #' @param SQL_table Optional. Name of the specific data to import from the GeoPackage. Defaults to "merged" in the case of the Hydrography90m stream network.
-#' @param dt If TRUE, import the GeoPackage as a data.table
-#' @param g If TRUE, import the GeoPackage as a directed graph (igraph object)
-#' @param sf If TRUE, import the GeoPackage as a spatial dataframe (sf object)
-#' @param SpatVect If TRUE, import the GeoPackage as a SpatVecteor (terra object)
+#' @param dt If TRUE, import the GeoPackage as a data.table.
+#' @param g If TRUE, import the GeoPackage as a directed graph (igraph object). Only possible for a network.
+#' @param sf If TRUE, import the GeoPackage as a spatial dataframe (sf object).
+#' @param SpatVect If TRUE, import the GeoPackage as a SpatVecteor (terra object).
 
 #' @importFrom DBI dbConnect dbListTables dbGetQuery
 #' @importFrom RSQLite SQLite
@@ -23,15 +24,18 @@
 
 
 # Import a geopackage from disk as a data.table
-read_geopackage <- function(filename, dt=F, g=F, sf=F, SpatVect=F, SQL_table="merged") {
+read_geopackage <- function(filename, type=NULL, dt=F, g=F, sf=F, SpatVect=F, SQL_table=NULL) {
 
-  # Test input argumnets
+  # Test input arguments
   if(missing(filename)) stop("Please specify the input geopackage file.")
   if(!grepl(".gpkg", filename, fixed = TRUE)) stop("Input must be a GeoPackage file.")
   # If all options are missing
   if(dt==FALSE & g==FALSE & sf==FALSE & SpatVect==FALSE) {
     stop("Please select one output type.\n")
   }
+  if(type!="net" & type!="catch") stop("Please specify 'net' or 'catch' as the input file type.")
+  if(type=="catch" & g==TRUE) stop("A graph can only be created from a network.")
+
 
   # Avoid exponential numbers in the table and IDs, only set this only within the function
   options(scipen=999)
@@ -44,7 +48,14 @@ read_geopackage <- function(filename, dt=F, g=F, sf=F, SpatVect=F, SQL_table="me
   cat("Opening SQL connection...\n")
   conn <- dbConnect(drv=RSQLite::SQLite(), dbname=filename) # get connection
   # SQL_table <- dbListTables(conn) # list all tables
-  # SQL_table <- "merged" # "SELECT"
+  if(missing(SQL_table)) {
+    if(type=="net") {
+      SQL_table <- "merged"
+        } else if(type=="catch") {
+          SQL_table <- "sub_catchment"
+        }
+  }
+
   cat("Reading GeoPackage file...\n")
   network_table <- dbGetQuery(conn=conn, statement=paste0("SELECT * FROM '", SQL_table, "'")) # Read the database
   setDT(network_table) # convert to data.table
@@ -59,14 +70,14 @@ read_geopackage <- function(filename, dt=F, g=F, sf=F, SpatVect=F, SQL_table="me
   if(dt==T & g==F) {
     return(network_table) }
 
-  if (g==T) {
+  if (g==T & type=="net") { # only for network
     cat("Building graph...\n")
     g_out <- graph_from_data_frame(network_table, directed = T)
     return(g_out) }
 
 } else if (sf==T) {
     cat("Importing as a sf spatial dataframe...\n")
-    sf <- read_sf(filename, as_tibble=F) # tibble=F to avoid exponential numbera
+    sf <- read_sf(filename, as_tibble=F) # tibble=F to avoid exponential numbers
     return(sf)  } else if (SpatVect==T) {
     cat("Importing as a terra SpatVect object...\n")
     sf <- read_sf(filename)
@@ -108,7 +119,14 @@ read_geopackage <- function(filename, dt=F, g=F, sf=F, SpatVect=F, SQL_table="me
 #
 # small file
 # filename="order_vect_segment_h00v00.gpkg"
-# my_table <- read_geopackage("order_vect_segment_h00v00.gpkg", dt=T)
+# filename="sub_catchment_h00v00.gpkg"
+# my_table <- read_geopackage("order_vect_segment_h00v00.gpkg", type="net", dt=T)
 # my_graph <- read_geopackage("order_vect_segment_h00v00.gpkg", g=T)
 # my_sf <- read_geopackage("order_vect_segment_h00v00.gpkg", sf=T)
 # my_vect <- read_geopackage("order_vect_segment_h00v00.gpkg", SpatVect=T)
+
+
+
+# my_table <- read_geopackage("sub_catchment_h00v00.gpkg", type="catch", SpatVect=T)
+
+
