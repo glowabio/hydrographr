c7349f6163e1192a6417eb7962980eabef24ddc1
domisch <domisch@igb-berlin.de>
2022-11-24 23:09:02 +0100

    Fixed join

--- R/segment_neighbours.R
+++ R/segment_neighbours.R
@@ -8,9 +8,9 @@
 #' @param segmentID The input segment IDs as a numerical vector for which to search the connected segments.
 #' @param order The neighbouring order. Order=1 would be immediate neighbours of the input segementID, order=2 would be the order 1 plus the immediate neighbours of the segementIDs in order 1.
 #' @param mode One of "in", "out", or "all". "in" reports only upstream neighbour segments, "out" reports only the downstream segments, and "all" does both.
-#' @param variable Optional. One or more attribute(s) or variable(s) of the input graph that should be reported for each output segmentID.
+#' @param variable Optional. One or more attribute(s) or variable(s) of the input graph that should be reported for each output segmentID ("to_stream").
 #' @param attach_only Logical. If TRUE then the selected variables will be only attached to each for each segment without any further aggregation.
-#' @param stat One of mean, median, min, max, sd (without quotes). Aggregates (or summarizes) the variables for the neighbourhood of each input stream segment (e.g., the average land cover in the next five upstream segments or sub-catchments).
+#' @param stat One of mean, median, min, max, sd (without quotes). Aggregates (or summarizes) the variables for the neighbourhood of each input segment ("stream", e.g., the average land cover in the next five upstream segments or sub-catchments).
 #' @param n_cores Optional. Specify the number of CPUs for internal parallelization in the case of multiple stream segments / outlets. Defaults to the all available CPUs minus two. In case the graph is very large, and many segments are used as an input, setting n_cores to 1 might might be useful to avoid any RAM errors, while still achieving a fast computation. This is because the large data will be copied to each CPU which might slow things down.
 #' @param maxsize Optional. Specify the maximum size of the data passed to the parallel backend in MB. Defaults to 1500 (1.5 GB). Consider a higher value for large study areas (more than one 20°x20° tile).
 
@@ -28,12 +28,9 @@
 
 
 
-
-
-
-
-segment_neighbours <- function(g, segmentID=NULL, variable=NULL, stat=NULL, attach_only=F, order=5, mode="in", n_cores=NULL, maxsize=1500) {
-
+segment_neighbours <- function(g, segmentID=NULL, variable=NULL, stat=NULL,
+                               attach_only=F, order=5, mode="in", n_cores=NULL,
+                               maxsize=1500) {
 
   # Check input arguments
   if ( class(g) != "igraph")     stop("Input must be an igraph object. Please run table_to_graph() first.")
@@ -72,7 +69,6 @@ segment_neighbours <- function(g, segmentID=NULL, variable=NULL, stat=NULL, atta
     if(get_os()=="windows") {
       plan(multisession, workers = n_cores)
     }
-
     if(get_os()=="osx" || get_os()=="linux")  {
       plan(multicore, workers = n_cores) # multicore?
     }
@@ -93,8 +89,7 @@ segment_neighbours <- function(g, segmentID=NULL, variable=NULL, stat=NULL, atta
 
   ### Get the path number as an additional column in the graph list.vs
   mapply_fun <- function(element,name){
-    mutate(element,stream = name)
-  }
+    mutate(element,stream = name) }
   ### Get the stream (from) ID as an additional column
   l <- future_mapply(mapply_fun,l,names(l),SIMPLIFY = F)
   # Get into one file
@@ -102,12 +97,7 @@ segment_neighbours <- function(g, segmentID=NULL, variable=NULL, stat=NULL, atta
   # Rename and set key
   names(dt)[1] <- "to_stream"
   setkey(dt, stream)
-  # Remove the from-stream, self-reference
-  dt <- dt[dt$stream != dt$to_stream,]
-  dt <- unique(dt) # remove any duplicates
-  # Set col order
-  setcolorder(dt, c("stream", "to_stream"))
-
+  dt <- unique(dt) # remove any duplicates, if any
 
   # If aggregation was defined:
   if(!missing(variable)) {
@@ -116,12 +106,18 @@ segment_neighbours <- function(g, segmentID=NULL, variable=NULL, stat=NULL, atta
   cat("Attaching the attribute(s)", variable, "\n")
   # Get the attributes for all edges of the full graph
   lookup_dt <- as.data.table(as_long_data_frame(g)[c("ver[el[, 1], ]", variable)])
-  names(lookup_dt)[1] <- "stream"
+  names(lookup_dt)[1] <- "to_stream"
 
   # Merge the network attributes and sort:
-  dt_join <- merge(dt, lookup_dt, by="stream", all.x=TRUE)
-  dt_join <- dt[lookup_dt, on="to_stream"] # lookup_dt[dt, on="stream"]  gives NAs
+  # dt_join <- merge(dt, lookup_dt, by="to_stream", all.x=TRUE)
+  # dt_join <- dt[lookup_dt, on="to_stream"] # lookup_dt[dt, on="stream"]  gives NAs
+  dt_join <- lookup_dt[dt, on="to_stream"]
   dt_join <- dt_join[order(-rank(stream))]
+  # Remove the from-stream, self-reference
+  dt_join <- dt_join[dt_join$stream != dt_join$to_stream,]
+  # Set col order
+  setcolorder(dt_join, c("stream", "to_stream", variable))
+
   # Export only attached data
   if(attach_only==TRUE) {
     return(dt_join)
@@ -161,7 +157,7 @@ segment_neighbours <- function(g, segmentID=NULL, variable=NULL, stat=NULL, atta
 # g <- my_catchment
 # segmentID=c(371901515)
 #
-segmentID=tail(as_ids(V(my_graph)))
+segmentID=(as_ids(V(my_graph)))
 
 
 out <- segment_neighbours(my_graph, segmentID=segmentID,
@@ -170,13 +166,13 @@ out <- segment_neighbours(my_graph, segmentID=segmentID,
                           attach_only=T)
 
 out <- segment_neighbours(my_graph, segmentID=segmentID,
-                          order=2, mode="in", n_cores=1,
+                          order=2, mode="out", n_cores=1,
                           variable=c("length", "source_elev"),
                           stat=median)
 
 
 out <- segment_neighbours(my_graph, segmentID=segmentID,
-                          order=2, mode="in", n_cores=1,
+                          order=2, mode="all", n_cores=1,
                           variable=c("length", "source_elev"),
                           stat=mean,
                           attach_only=T)
