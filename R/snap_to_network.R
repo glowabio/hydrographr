cc79997a9a1bd2a54c9261d56f62c75ebfecacfb
mueblacker <m.ueblacker@posteo.at>
2022-11-24 19:03:47 +0100

    Fix path, add comments, change description

--- R/snap_to_network.R
+++ R/snap_to_network.R
@@ -7,11 +7,11 @@
 #' @param lon Name of the longitude column as character string
 #' @param lat Name of the latitude column as character string
 #' @param stream_path Full path to the stream network .tif file
-#' @param radius Radius distance in meters; Point will be snapped to the next
+#' @param radius Maximum distance in meters; Point will be snapped to the next
 #' stream within a certain distance.
 #' @param flow_path Full path to the flow accumulation .tif file.
-#' @param thre Flow accumulation threshold value; Point will be snapped to the next
-#' stream with a flow accumulation equal or higher than threshold value.
+#' @param thre Minimum flow accumulation; Point will be snapped to the next
+#' stream with a flow accumulation equal or higher than the given value.
 #' @importFrom stringi stri_rand_strings
 #' @importFrom dplyr select
 #' @importFrom data.table fread
@@ -19,6 +19,8 @@
 #' @export
 #'
 
+# Note change to one path and one threshold argument or make it possible to
+# calculate both snapping at once
 
 snap_to_network <- function(data, lon, lat, stream_path = NULL, radius = 500,
                             flow_path = NULL, thre = 0.5, quiet = TRUE) {
@@ -26,11 +28,6 @@ snap_to_network <- function(data, lon, lat, stream_path = NULL, radius = 500,
   system <- get_os()
   # Make bash scripts executable
   make_sh_exec()
-
-  # Convert NULL argument to "NA" so that the bash script can evaluate
-  # the argument
-  stream_path <- ifelse(is.null(stream_path), "NA", stream_path)
-  flow_path <- ifelse(is.null(flow_path), "NA", flow_path)
 
   # Create random string to attach to the file name of the temporary
   # output coordinates and input ids file
@@ -48,12 +45,17 @@ snap_to_network <- function(data, lon, lat, stream_path = NULL, radius = 500,
 
   if (system == "linux" || system == "osx"){
 
-  run(system.file("sh", "snap_to_network.sh",
-                  package = "hydrographr"),
-      args = c(coord_tmp_path, lon, lat,
-               stream_path, radius, flow_path, thre,
-               snap_tmp_path, tempdir()),
-      echo = !quiet)
+    # Convert NULL argument to "NA" so that the bash script can evaluate
+    # the argument
+    stream_path <- ifelse(is.null(stream_path), "NA", stream_path)
+    flow_path <- ifelse(is.null(flow_path), "NA", flow_path)
+
+    run(system.file("sh", "snap_to_network.sh",
+                    package = "hydrographr"),
+        args = c(coord_tmp_path, lon, lat,
+                 stream_path, radius, flow_path, thre,
+                 snap_tmp_path, tempdir()),
+        echo = !quiet)
 
 
   } else {
@@ -62,9 +64,9 @@ snap_to_network <- function(data, lon, lat, stream_path = NULL, radius = 500,
     check_wsl()
     # Change path for WSL
     wsl_coord_tmp_path <- fix_path(coord_tmp_path)
-    wsl_stream_path <- fix_path(stream_path)
-    wsl_flow_path <- fix_path(flow_path)
-    wsl_snap_tmp_path <- fix_path(ids_tmp_path)
+    wsl_stream_path <- ifelse(is.null(stream_path), "NA", fix_path(stream_path))
+    wsl_flow_path <- ifelse(is.null(flow_path), "NA", fix_path(flow_path))
+    wsl_snap_tmp_path <- fix_path(snap_tmp_path)
     wsl_tmp_path <- fix_path(tempdir())
     wsl_sh_file <- fix_path(system.file("sh", "snap_to_network.sh",
                                         package = "hydrographr"))
@@ -80,6 +82,9 @@ snap_to_network <- function(data, lon, lat, stream_path = NULL, radius = 500,
                              keepLeadingZeros = TRUE, header = TRUE, sep = " ")
 
   # Remove files in the tmp folder
-  file.remove(snap_tmp_path)
+  file.remove(coord_tmp_path, snap_tmp_path)
+
+  # Return snapped coordinates
+  return(snapped_coord)
 
 }
