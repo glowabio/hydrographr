56cbd5bd9ff7b78334cad9036093a176eb2cad32
Thomas Tomiczek <ttomiczek@AD.IGB-BERLIN.DE>
2022-11-24 10:15:07 +0100

    Integration of output_path
    Choice of the output path has been included into the script and windows integration.

--- R/merge_tile.R
+++ R/merge_tile.R
@@ -1,17 +1,53 @@
 #' Merge raster or spatial vector tiles
 #'
 #' @param tile_path Path to raster tile or spatial vector
+#' @param output_path Path to write the output
 #' @importFrom processx run
+#' @importFrom terra rast
+#' @importFrom terra vect
+#' @param rraster_read If TRUE merged raster .tif layer gets read into R.
+#' TRUE is set by default.
+#' @param rvector_read If TRUE merged spatial vector gets read into R.
+#' FALSE is set by default.
 #' @export
 #'
 
-merge_tiles <- function(tile_path) {
-	 if (missing(tile_path) || is.na(tile_path)) {
-   tile_path <- "NOT_ASSIGNED" 
-   } else {
- 	merge_tiles <- run(system.file("sh", "merge_tiles.sh",
+merge_tiles <- function(tile_path, output_path, rmerge_read == TRUE) {
+	if (missing(tile_path) || is.na(tile_path)) {
+    tile_path <- "NOT_ASSIGNED" 
+    } else {
+      # Check operating system
+      system <- get_os()
+      if (system == "linux") {
+ 	  merge_tiles <- run(system.file("sh", "merge_tiles.sh",
                            package = "hydrographr"),
                    args = c(tile_path),
-                   echo = FALSE)$stdout }
- 	 return(merge_tiles)
+                   echo = FALSE)$stdout
+
+       } else if (system == "windows") {
+       # Check if WSL and Ubuntu are installed
+       check_wsl()
+       # Change paths for WSL
+       wsl_tile_path <- fix_path(tile_path)
+       wsl_output_path <- fix_path(output_path)
+       wsl_sh_mer_file <- fix_path(
+         system.file("sh", "merge_tiles.sh",
+                    package = "hydrographr"))
+       }
+    }
+
+ 	if (rraster_read == TRUE) {
+      # Read merged .tif layer
+      merge_tiles <- rast(output_path)
+    }
+    
+    if (rvector_read == FALSE) {
+      # Read merged vector layer
+      merge_tiles <- vect(output_path) 
+
+      return(merge_tiles)
+    } else {
+      # Print message
+      print(paste0("Merge saved under: ", output_path))
+    }
  }
--- inst/sh/merge_tiles.sh
+++ inst/sh/merge_tiles.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
 export BAS=$1
+export OUT=$2
 ## $1 is the raster tile or spatial vector path
+## $2 is the output path
 
 for i in "$BAS"; do
-	 gdalbuildvrt basin.vrt $BAS/*.tif 
-	 gdal_translate -co COMPRESS=DEFLATE -co ZLEVEL=9 basin.vrt  basin.tif
+	# merge raster file by first creating a vrt and then do the merge
+	 gdalbuildvrt basin.vrt $BAS/*.tif
+	 rm -f basin.tif
+	 gdal_translate -co COMPRESS=DEFLATE -co ZLEVEL=9 basin.vrt $OUT/basin.tif
 	 rm -f basin.vrt
+
+	 # merge vector file
 	 ogrmerge.py -single -progress -skipfailures -overwrite_ds -f GPKG -o basin.gpkg  $BAS/*.gpkg 
 	 rm -f basin_dissolved.gpkg  
-	 ogr2ogr  -nlt POLYGON -dialect sqlite -sql "SELECT ST_Union(geom),"ID" FROM merged GROUP BY "ID" " basin_dissolved.gpkg basin.gpkg
-	 rm -f basin.gpkg  
+	 ogr2ogr  -nlt POLYGON -dialect sqlite -sql "SELECT ST_Union(geom),"ID" FROM merged GROUP BY "ID" " $OUT/basin_dissolved.gpkg basin.gpkg
+	 rm -f basin.gpkg 
 done
