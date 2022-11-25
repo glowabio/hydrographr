1b4652b0a4181a0bc17eb51b51f300ce26af5bb9
Jaime Garcia Marquez <jrgarcia.marquez@gmail.com>
2022-11-25 12:39:33 +0100

    select either option (distance or accumulation flow) or both

--- inst/sh/snap_to_network.sh
+++ inst/sh/snap_to_network.sh
@@ -26,6 +26,9 @@ export SNAP=$8
 ## Temporary folder
 export DIR=$9
 
+## What to calculate
+export CALC=${10}
+
 ## Set random string
 export RAND_STRING=$(xxd -l 8 -c 32 -p < /dev/random)
 
@@ -55,30 +58,97 @@ v.in.ogr --o input=$DIR/ref_points_${RAND_STRING}.gpkg layer=ref_points output=r
 r.in.gdal input=$STR output=stream
 
 
-if [ "$ACC" != "NA"  ]
-then 
+if [ "$CALC" = "dist" ]
+then
+    r.stream.snap --o input=ref_points output=snap_points stream_rast=stream \
+        radius=$rdist
+
+    r.what map=stream points=snap_points separator=comma \
+        | awk -F, '{print $4}' > $DIR/stream_ID_${RAND_STRING}.txt
+
+    v.out.ascii -c input=snap_points separator=space | awk '{print $1, $2}' \
+        > $DIR/snap_coords_${RAND_STRING}.txt
+
+    sed -i 's/east/lon_snap/g' $DIR/snap_coords_${RAND_STRING}.txt
+    sed -i 's/north/lat_snap/g' $DIR/snap_coords_${RAND_STRING}.txt
+
+    cat  $DATA | tr -s ',' ' ' > $DIR/coords_${RAND_STRING}.txt
+    paste -d" " $DIR/coords_${RAND_STRING}.txt $DIR/snap_coords_${RAND_STRING}.txt \
+        <(printf "%s\n" subc_id_snap $(cat $DIR/stream_ID_${RAND_STRING}.txt))  \
+        >  $SNAP
+    
+    rm $DIR/snap_coords_${RAND_STRING}_*.txt $DIR/stream_ID_${RAND_STRING}_*.txt \
+        $DIR/ref_points_${RAND_STRING}.gpkg 
+fi
+
+
+if [ "$CALC" = "accu" ]
+then
     r.in.gdal input=$ACC output=accum
     r.stream.snap --o input=ref_points output=snap_points stream_rast=stream \
         radius=$rdist accumulation=accum threshold=$acct
-else
-    r.stream.snap --o input=ref_points output=snap_points stream_rast=stream \
-        radius=$rdist 
+
+    r.what map=stream points=snap_points separator=comma \
+        | awk -F, '{print $4}' > $DIR/stream_ID_${RAND_STRING}.txt
+
+    v.out.ascii -c input=snap_points separator=space | awk '{print $1, $2}' \
+        > $DIR/snap_coords_${RAND_STRING}.txt
+
+    sed -i 's/east/lon_snap/g' $DIR/snap_coords_${RAND_STRING}.txt
+    sed -i 's/north/lat_snap/g' $DIR/snap_coords_${RAND_STRING}.txt
+
+    cat  $DATA | tr -s ',' ' ' > $DIR/coords_${RAND_STRING}.txt
+    paste -d" " $DIR/coords_${RAND_STRING}.txt $DIR/snap_coords_${RAND_STRING}.txt \
+        <(printf "%s\n" subc_id_snap $(cat $DIR/stream_ID_${RAND_STRING}.txt))  \
+        >  $SNAP
+    
+    rm $DIR/snap_coords_${RAND_STRING}_*.txt $DIR/stream_ID_${RAND_STRING}_*.txt \
+        $DIR/ref_points_${RAND_STRING}.gpkg 
 fi
 
-r.what map=stream points=snap_points separator=comma \
-    | awk -F, '{print $4}' > $DIR/stream_ID_${RAND_STRING}.txt
-v.out.ascii -c input=snap_points separator=space | awk '{print $1, $2}' \
-    > $DIR/snap_coords_${RAND_STRING}.txt
+
+if [ "$CALC" = "both" ]
+then
+
+    r.stream.snap --o input=ref_points output=snap_points_d stream_rast=stream \
+        radius=$rdist
+
+    r.in.gdal input=$ACC output=accum
+    r.stream.snap --o input=ref_points output=snap_points_a stream_rast=stream \
+        radius=$rdist accumulation=accum threshold=$acct
+
+    r.what map=stream points=snap_points_d separator=comma \
+        | awk -F, '{print $4}' > $DIR/stream_ID_${RAND_STRING}_d.txt
+    
+    r.what map=stream points=snap_points_a separator=comma \
+        | awk -F, '{print $4}' > $DIR/stream_ID_${RAND_STRING}_a.txt
+
+    v.out.ascii -c input=snap_points_d separator=space | awk '{print $1, $2}' \
+        > $DIR/snap_coords_${RAND_STRING}_d.txt
+    
+    v.out.ascii -c input=snap_points_a separator=space | awk '{print $1, $2}' \
+        > $DIR/snap_coords_${RAND_STRING}_a.txt
+
+    sed -i 's/east/lon_snap/g' $DIR/snap_coords_${RAND_STRING}_d.txt
+    sed -i 's/north/lat_snap/g' $DIR/snap_coords_${RAND_STRING}_d.txt
+    
+    sed -i 's/east/lon_snap/g' $DIR/snap_coords_${RAND_STRING}_a.txt
+    sed -i 's/north/lat_snap/g' $DIR/snap_coords_${RAND_STRING}_a.txt
+
+    cat  $DATA | tr -s ',' ' ' > $DIR/coords_${RAND_STRING}.txt
+
+    paste -d" " $DIR/coords_${RAND_STRING}.txt \
+        $DIR/snap_coords_${RAND_STRING}_d.txt \
+        <(printf "%s\n" subc_id_snap_d $(cat $DIR/stream_ID_${RAND_STRING}_d.txt))  \
+        $DIR/snap_coords_${RAND_STRING}_a.txt \
+        <(printf "%s\n" subc_id_snap_a $(cat $DIR/stream_ID_${RAND_STRING}_a.txt))  \
+        >  $SNAP
+
+    rm $DIR/snap_coords_${RAND_STRING}_*.txt $DIR/stream_ID_${RAND_STRING}_*.txt \
+        $DIR/ref_points_${RAND_STRING}.gpkg 
+fi
+
+
 EOF
 
-sed -i 's/east/lon_snap/g' $DIR/snap_coords_${RAND_STRING}.txt
-sed -i 's/north/lat_snap/g' $DIR/snap_coords_${RAND_STRING}.txt
 
-cat  $DATA | tr -s ',' ' ' > $DIR/coords_${RAND_STRING}.txt
-paste -d" " $DIR/coords_${RAND_STRING}.txt $DIR/snap_coords_${RAND_STRING}.txt \
-    <(printf "%s\n" subc_id_snap $(cat $DIR/stream_ID_${RAND_STRING}.txt))  \
-    >  $SNAP
-
-EOF
-
-rm $DIR/snap_coords_${RAND_STRING}.txt $DIR/stream_ID_${RAND_STRING}.txt $DIR/ref_points_${RAND_STRING}.gpkg #$DIR/$DATC
