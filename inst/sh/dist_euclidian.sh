7160288602fb544946627f0e113d4da786fbed70
Jaime Garcia Marquez <jrgarcia.marquez@gmail.com>
2022-12-05 11:40:26 +0100

    initial commit

--- inst/sh/dist_euclidian.sh
+++ inst/sh/dist_euclidian.sh
@@ -15,23 +15,22 @@ export DATA=$2
 export LON=$3
 export LAT=$4
 
-# full path and name of output file $DIR/pairwise_euclidian_dist.txt
-export OUT=$5
 
 ################################
 ################################
 
+###   if input is space separated, we need to create a .csv
 ## save name of file without extension
-export b=$(echo $DATA | awk -F"." '{print $1}')
-
-## if the file is not csv, add the comma and make it .csv
-if [ "${DATA: -4}" != ".csv" ]
-then
-    cat  $DATA | tr -s '[:blank:]' ',' > ${b}.csv
-    export DATAC=$(echo ${b}.csv)
-else
-    DATAC=$DATA
-fi
+#export b=$(echo $DATA | awk -F"." '{print $1}')
+#
+### if the file is not csv, add the comma and make it .csv
+#if [ "${DATA: -4}" != ".csv" ]
+#then
+#    cat  $DATA | tr -s '[:blank:]' ',' > ${b}.csv
+#    export DATAC=$(echo ${b}.csv)
+#else
+#    DATAC=$DATA
+#fi
 
 ##  make the file a gpkg
 ogr2ogr -f "GPKG" -overwrite -nln ref_points -nlt POINT -a_srs EPSG:4326 \
@@ -51,7 +50,7 @@ output=allpoints type=point key=$SITE
 #  Calculate distance, results are given in meters
 v.distance -pa from=allpoints to=allpoints upload=dist separator=comma \
     | sed -re 's/([0-9]+\.[0-9]{2})[0-9]+/\1/g' | \
-    awk -F, 'NR > 1 {print $0}' > $OUT 
+    awk -F, 'NR > 1 {print $0}' > $DIR/dist_euclidian.txt 
 
 EOF
 
