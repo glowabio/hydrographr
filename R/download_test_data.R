964a762112e2d6f5cef966e8755e70096c0a9be8
Sami Domisch <domisch@igb-berlin.de>
2022-12-06 21:32:34 +0100

    Update download_test_data.R
    Fixed zip in destination path

--- R/download_test_data.R
+++ R/download_test_data.R
@@ -11,7 +11,7 @@
 #' download_test_data()
 #'
 
-download_test_data <- function(download_path = "./hydrography90m_test_data.zip") {
+download_test_data <- function(download_path = "./hydrography90m_test_data") {
 
   # General path to the test data folder in GDrive
   gdrive_path <- "https://docs.google.com/uc?export=download&id="
