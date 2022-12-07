a348a9c671438722c13acb1ceec269341474d8bc
afrogri37 <afroditi.grigoropoulou@igb-berlin.de>
2022-12-07 13:55:22 +0100

    fixed path and download.files option

--- R/download_test_data.R
+++ R/download_test_data.R
@@ -1,8 +1,14 @@
-#' Downloads all the test data from https://drive.google.com/drive/u/1/folders/18RpqmywL7yThitsOklWXAuYIfMvfyPfg.
+#' Downloads the test data for the Hydrography90m dataset
+#'
+#' The test data is available at from https://drive.google.com/drive/u/1/folders/18RpqmywL7yThitsOklWXAuYIfMvfyPfg and can b downloaded with this function.
 #'
 #' @param download_path The path where the files will be downloaded
 #' @author Afroditi Grigoropoulou
 #' @export
+#'
+#' @examples
+#' # Download the test data to the current working directory
+#' download_test_data()
 #'
 
 download_test_data <- function(download_path = "./hydrography90m_test_data") {
@@ -11,9 +17,20 @@ download_test_data <- function(download_path = "./hydrography90m_test_data") {
   gdrive_path <- "https://docs.google.com/uc?export=download&id="
 
   # File id of the zipped folder retrieved from gdrive
-  file_id <- "17qxc0t8PqnzuUzOtfXytwX8_qwNrfN6s"
+  file_id <- "1kYNWXmtVm6X7MZLISOePGpvxB1pk1scD"
 
   download.file(paste0(gdrive_path, file_id, "&confirm=t"),
-                destfile = download_path)
+                destfile = paste0(download_path, ".zip"), mode = "wb")
+
+  # Unzip the data
+  unzip(paste0(download_path, ".zip"),  overwrite = T,
+        exdir = download_path,
+        unzip=getOption("unzip"))
+
+  # remove the zip file
+  unlink(paste0(download_path, ".zip"))
+
+  # Report
+  cat("Data downloaded and unzipped to", download_path, "\n")
 
 }
