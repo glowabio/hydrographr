f031e11722fa2d9bd35dfe74f32c3abbfd4d2779
mueblacker <m.ueblacker@posteo.at>
2023-01-02 17:37:51 +0100

    Create batch file for set_no_data function

--- inst/bat/set_no_data.bat
+++ inst/bat/set_no_data.bat
@@ -1,9 +1,7 @@
-@echo on
+@echo off
 
-set WSLENV=%1 %2 %3 %4 %5 %6
-set DATA=%1
-set SHDIR=%6
+set WSLENV=%1 %2 %3 %4
+set SHDIR=%4
 
-wsl dos2unix %DATA%
 wsl dos2unix %SHDIR% 
 wsl bash  %SHDIR% $WSLENV
