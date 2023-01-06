afed991cc25820aae2c05da1960477d6586112d0
mueblacker <m.ueblacker@posteo.at>
2023-01-06 12:25:03 +0100

    Fixed code for Windows, add error messages, add example, reference, and authors

--- R/get_upstream_catchment.R
+++ R/get_upstream_catchment.R
@@ -5,9 +5,11 @@
 #' @param lon Name of the longitude column (WGS 84)
 #' @param lat Name of the latitude column (WGS 84)
 #' @param direction_layer Full path to raster file with the direction variable
-#' @param out_path Full path to the directory where the output(s) will be stored
-#' @param n_cores Numeric. Number of cores used for parallelization
+#' @param out_path Full path to the directory where the output(s) will be stored.
+#' To identify the upstream catchment the output file name includes the site id.
+#' @param n_cores Numeric. Number of cores used for parallelization.
 #' @param quiet If FALSE, process is printed
+#'
 #' @importFrom stringi stri_rand_strings
 #' @importFrom dplyr select
 #' @importFrom data.table fread fwrite
@@ -15,15 +17,132 @@
 #' @importFrom parallel detectCores
 #' @export
 #'
+#' @author Jaime Garcia Marquez, Afroditi Grigoropoulou, Maria M. Üblacker
 #'
-get_upstream_catchment <- function(data, id = character(0), lon = character(0),
-                        lat = character(0), direction_layer = NULL,
-                        out_path = NULL, n_cores = NULL, quiet = TRUE) {
+#' @references
+#' \url{https://grass.osgeo.org/grass82/manuals/r.water.outlet.html}
+#' \url{https://grass.osgeo.org/grass82/manuals/r.region.html}
+#'
+#' @seealso
+#' \code{\link{snap_to_network}} to snap the data points to the next stream
+#' segment within a given radius and/or a given flow accumulation threshold
+#' value.
+#' \code{\link{snap_to_subc_segment}} to snap the data points to the next stream
+#' segment within the sub-catchment the point is located.
+#' \code{\link{extract_ids}} to extract basin and sub-catchment IDs.
+#'
+#' @examples
+#' # Download test data into temporary R folder
+#' my_directory <- tempdir()
+#' download_test_data(my_directory)
+#'
+#' # Before running the function get_upstream_catchment(), snap the points to
+#' # to the stream segment. There are multiple ways to snap the points. Here is
+#' # one example:
+#'
+#' # Load occurrence data
+#' species_occurence <- read.table(paste0(my_directory,
+#'                                 "/hydrography90m_test_data/spdata_1264942.txt"),
+#'                                 header = TRUE)
+#'
+#' # Define full path to the basin and sub-catchments raster layer
+#' basin_rast <- paste0(my_directory,
+#'                      "/hydrography90m_test_data/basin_1264942.tif")
+#' subc_rast <- paste0(my_directory,
+#'                     "/hydrography90m_test_data/subcatchment_1264942.tif")
+#'
+#' # Define full path to the vector file of the stream network
+#' stream_vect <- paste0(my_directory,
+#'                       "/hydrography90m_test_data/order_vect_59.gpkg")
+#'
+#' # Automatically extract the basin and sub-catchment IDs and
+#' # snap the data points to the stream segment
+#' snapped_coordinates <- snap_to_subc_segment(data = species_occurence,
+#'                                             lon = "longitude",
+#'                                             lat = "latitude",
+#'                                             site_id = "occurrence_id",
+#'                                             basin_path = basin_rast,
+#'                                             subc_path = subc_rast,
+#'                                             stream_path = stream_vect,
+#'                                             cores = 2)
+#'
+#' # Define full path to the direction .tif
+#' direction_rast <- paste0(my_directory,
+#'                         "/hydrography90m_test_data/direction_1264942.tif")
+#' # Define the path for the output file(s)
+#' output <-  paste0(my_directory, "/upstream_catchments")
+#' if(!dir.exists(output)) dir.create(output)
+#' # Get the upstream catchment for each point location
+#' get_upstream_catchment(hydrography90m_ids,
+#'                        lon = "longitude",
+#'                        lat = "latitude",
+#'                        id = "occurrence_id",
+#'                        direction_layer = dir_rast,
+#'                        out_path = output,
+#'                        n_cores = 2)
+#'
 
-  system <- get_os()
+get_upstream_catchment <- function(data, id, lon, lat, direction_layer = NULL,
+                                   out_path = NULL, n_cores = NULL,
+                                   quiet = TRUE) {
 
-  # Make bash scripts executable
-  make_sh_exec()
+  # Check if data.frame is defined
+  if (missing(data))
+    stop("data: Input data.frame is missing.")
+
+  # Check if input data is of type data.frame,
+  # data.table or tibble
+  if (!is(data, "data.frame"))
+    stop("data: Has to be of class 'data.frame'.")
+
+  # Check if column name is defined
+  if (missing(lon))
+    stop("Column name of longitudinal coordinate is not defined.")
+
+  # Check if column name is defined
+  if (missing(lat))
+    stop("Column name of latitudinal coordinate is not defined.")
+
+  # Check if lon/lat column names are character vectors
+  if (!is.character(lon))
+    stop("lon: Column name is not a character vector.")
+  if (!is.character(lat))
+    stop("lat: Column name is not a character vector.")
+  if (!is.character(id))
+    stop("id: Column name is not a character vector.")
+
+  # Check if lon/lat column names exist
+  if (is.null(data[[lon]]))
+    stop(paste0("Column name '", lon, "' does not exist."))
+  if (is.null(data[[lat]]))
+    stop(paste0("Column name '", lat, "' does not exist."))
+  if (is.null(data[[id]]))
+    stop(paste0("Column name '", id, "' does not exist."))
+
+  # Check if values of the lon/lat columns are numeric
+  if (!is.numeric(data[[lon]]))
+    stop(paste0("Column ", lon, " has to be numeric."))
+  if (!is.numeric(data[[lat]]))
+    stop(paste0("Column ", lat, " has to be numeric."))
+
+  # Check if direction_layer exists
+  if (!file.exists(direction_layer))
+    stop(paste0("direction_layer: ", direction_layer, " does not exist."))
+  # Check if accu_path exists
+    if (!dir.exists(out_path))
+      stop(paste0("out_path: ", out_path, " does not exist."))
+
+  # Check if direction_layer ends with .tif
+  if (!endsWith(direction_layer, ".tif"))
+    stop("direction_layer: Stream network raster is not a .tif file.")
+
+  # Check if quiet is logical
+  if (!is.logical(quiet))
+    stop("quiet: Has to be TRUE or FALSE.")
+
+  # Check if value of cores numeric
+  if (!is.numeric(n_cores))
+    stop("n_cores: Value has to be numeric.")
 
   # Create random string to attach to the file name of the temporary
   # points_dataset.txt and ids.txt file
@@ -41,17 +160,22 @@ get_upstream_catchment <- function(data, id = character(0), lon = character(0),
   # Setting up parallelization if n_cores is not provided
   n_cores <- ifelse(is.null(n_cores), detectCores() - 1, n_cores)
 
+  # Check operating system
+  system <- get_os()
+
+  # Make bash scripts executable
+  make_sh_exec()
+
 
   if (system == "linux" | system == "osx"){
-
 
     # Call the external .sh script get_upstream_catchment() containing the
     # GRASS functions
     processx::run(system.file("sh", "get_upstream_catchment.sh",
-        package = "hydrographr"),
-        args = c(coord_tmp_path, id, lon, lat, direction_layer, out_path,
-                 n_cores),
-        echo = !quiet)
+                              package = "hydrographr"),
+                  args = c(coord_tmp_path, id, lon, lat, direction_layer,
+                           out_path, n_cores),
+                  echo = !quiet)
 
   } else {
     # Check if WSL and Ubuntu is installed
@@ -60,6 +184,7 @@ get_upstream_catchment <- function(data, id = character(0), lon = character(0),
     wsl_coord_tmp_path <- fix_path(coord_tmp_path)
     wsl_dire_path <- ifelse(is.null(direction_layer), 0,
                             fix_path(direction_layer))
+    wsl_out_path <- fix_path(out_path)
     wsl_sh_file <- fix_path(
       system.file("sh", "get_upstream_catchment.sh",
                   package = "hydrographr"))
@@ -67,7 +192,7 @@ get_upstream_catchment <- function(data, id = character(0), lon = character(0),
     run(system.file("bat", "get_upstream_catchment.bat",
                                 package = "hydrographr"),
                     args = c(wsl_coord_tmp_path, id, lon, lat, wsl_dire_path,
-                             wsl_bas_path, wsl_tmp_path, n_cores, wsl_sh_file),
+                             wsl_out_path, n_cores, wsl_sh_file),
                     echo = !quiet)
 
   }
