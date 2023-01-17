69e54dc5abc8d3d2b5fb6671bbe4fc692a8cbe0e
mueblacker <m.ueblacker@posteo.at>
2023-01-17 15:45:57 +0100

    Add example code add error messages remove one if-else loop

--- R/merge_tile.R
+++ R/merge_tile.R
@@ -4,16 +4,15 @@
 #' to form a new raster or spatial vector object with a larger spatial extent.
 #' A directory with at least a raster .tif or spatial vector geopackage file
 #' must be provided.
-#' If read`_`raster = TRUE (default), the outputs are a .tif
-#' (saved under out`_`dir) and a SpatRaster (terra package) object,
-#' otherwise if read`_`raster = FALSE, the .tif file is the only output.
-#' If read`_`vector = TRUE, the outputs are a .gpkg (saved under out`_`dir)
-#' and a SpatVector (terra package) object,
-#' otherwise if read`_`vector = FALSE (default),
-#' the .gpkg file is the only output.
+#' If read_raster = TRUE (default), the outputs are a .tif
+#' (saved under out_dir) and a SpatRaster (terra package) object, otherwise if
+#' read_raster = FALSE, the .tif file is the only output. If read_vector = TRUE,
+#' the outputs are a .gpkg (saved under out_dir) and a SpatVector
+#' (terra package) object, otherwise if read_vector = FALSE (default), the .gpkg
+#' file is the only output.
 
-#' @param tile_dir character. The directory containing the raster tiles or
-#' spatial vectors to be merged.
+#' @param tile_dir character. The directory containing only the raster or
+#' spatial vectors tiles, which should be merged.
 #' @param out_dir character. The directory where the output will be stored.
 #' @param read_raster logical. If TRUE, the merged raster .tif layer gets read
 #' into R. If FALSE, the layer is only stored on disk. Default is TRUE.
@@ -25,37 +24,66 @@
 #' @importFrom terra vect
 #' @export
 #'
+#' @references
+#' \url{https://gdal.org/programs/gdalbuildvrt.html}
+#'
+#' \url{https://gdal.org/programs/gdal_translate.html}
+#'
+#' \url{https://gdal.org/programs/ogrmerge.html#ogrmerge}
+#'
+#' \url{https://gdal.org/programs/ogr2ogr.html}
+#'
+#' @author Thomas Tomiczek
+#'
 #' @return A .tif raster file or spatial vector object that is always written
 #' to disk, and optionally loaded into R.
 #'
 #' @examples
+#' # Download tiles into temporary R folder
+#' # or define a different directory
+#' my_directory <- tempdir()
+#' download_tiles(variable = "basin",
+#'                file_format = "tif",
+#'                tile_id = c("h22v08","h22v10"),
+#'                download_dir = my_directory)
 #'
+#' # Define folder containing only the tiles, which should me merged
+#' tiles_folder <- paste0(my_directory, "/r.watershed/basin_tiles20d")
+#' # Define output folder
+#' output_folder <- paste0(my_directory, "/merged_tiles")
+#' # Creat ouput folder if it doesn't exist
+#' if(!dir.exists(output_folder)) dir.create(output_folder)
 #'
-#'
-#' @author Thomas Tomiczek
+#' # Merge tiles
+#' merged_tiles <- merge_tiles(tile_dir = tiles_folder,
+#'                             out_dir = output_folder)
 #'
 
 merge_tiles <- function(tile_dir, out_dir,
-read_raster = TRUE, read_vector = FALSE) {
+                        read_raster = TRUE, read_vector = FALSE) {
+  # Check if paths exists
+  if (!dir.exists(tile_dir))
+    stop(paste0("Path: ", tile_dir, " does not exist."))
 
-  # Make bash scripts executable
-  make_sh_exec()
+  if (!dir.exists(out_dir))
+    stop(paste0("Path: ", out_dir, " does not exist."))
 
-  if (missing(tile_dir) || is.na(tile_dir)) {
-    tile_dir <- "NOT_ASSIGNED"
-    }
-  if (missing(out_dir) || is.na(out_dir)) {
-    out_dir <- "NOT_ASSIGNED"
-    } else {
+  if (read_raster == TRUE && read_vector == TRUE)
+    stop("One of read_raster and read_vector has to be FALSE")
+
       # Check operating system
       system <- get_os()
-      if (system == "linux") {
+
+      # Make bash scripts executable
+      make_sh_exec()
+
+      if (system == "linux" || system == "osx") {
         merge_tiles <- processx::run(system.file("sh", "merge_tiles.sh",
                             package = "hydrographr"),
                     args = c(tile_dir, out_dir),
                     echo = FALSE)
 
-       } else if (system == "windows") {
+       } else {
        # Check if WSL and Ubuntu are installed
        check_wsl()
        # Change path for WSL
@@ -68,9 +96,9 @@ read_raster = TRUE, read_vector = FALSE) {
        processx::run(system.file("bat", "merge_tiles.bat",
                     package = "hydrographr"),
         args = c(wsl_tile_dir, wsl_out_dir, wsl_sh_file),
-        echo = FALSE)
+        echo = TRUE)
        }
-    }
+
 
   if (read_raster == TRUE) {
       # Print message
