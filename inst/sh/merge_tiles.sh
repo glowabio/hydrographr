d7785cf39333f4b88361c2ada027be3305b6e2f1
Jaime Garcia Marquez <jrgarcia.marquez@gmail.com>
2023-01-19 10:50:07 +0100

    editions to merge function

--- inst/sh/merge_tiles.sh
+++ inst/sh/merge_tiles.sh
@@ -1,22 +1,61 @@
 #!/bin/sh
 
-export BAS=$1
-export OUT=$2
-## $1 is the raster tile or spatial vector path
-## $2 is the output path
+# directory where input files are located
+export dir=$1
 
- if (find "$BAS" -name "*tif") | grep -q $BAS; then 
-	# merge raster file by first creating a vrt and then do the merge
-	 gdalbuildvrt -overwrite $BAS/basin.vrt $BAS/*.tif 
-	 rm -f $BAS/basin.tif 
-	 gdal_translate -co COMPRESS=DEFLATE -co ZLEVEL=9 $BAS/basin.vrt $OUT/basin.tif
-	 rm -f $BAS/basin.vrt
+# list of file names (tif or gpkg) separated by commas
+export files=$2
 
- elif (find "$BAS" -name "*gpkg") | grep -q $BAS; then
-	 # merge vector file
-	 rm -f $BAS/basin.gpkg
-	 ogrmerge.py -single -progress -skipfailures -overwrite_ds -f GPKG -o $BAS/basin.gpkg  $BAS/*.gpkg 
-	 rm -f $BAS/basin_dissolved.gpkg  
-	 ogr2ogr  -nlt POLYGON -dialect sqlite -sql "SELECT ST_Union(ST_MakeValid(geom)),"ID" FROM merged GROUP BY "ID" " $OUT/basin_dissolved.gpkg $BAS/basin.gpkg
-	 rm -f $BAS/basin.gpkg
- fi
+# directory for output file
+export out=$3
+
+# output file name (including the tif or the gpkg extension)
+export outname=$4
+
+# remove the output file if exist
+[[ -f $out/${outname}  ]] && rm $out/${outname}
+
+# create an array  of all files needed (with complete path)
+g=( $(echo $files | sed 's/,/ /g') )
+farray=( $(for i in ${g[@]}; do find $dir -name "$i"; done) )
+
+
+# merge
+f=${farray[0]}
+if [[ ${f: -4} == ".tif" ]] 
+then
+    gdalbuildvrt -overwrite $out/merge.vrt ${farray[@]}
+    gdal_translate  -co COMPRESS=DEFLATE -co ZLEVEL=9  $out/merge.vrt $out/${outname}
+    rm $out/merge.vrt
+else
+    ogrmerge.py -single -progress -skipfailures -overwrite_ds -f GPKG \
+        -o $out/merge.gpkg ${farray[@]}
+    ogr2ogr  -nlt POLYGON -dialect sqlite \
+        -sql "SELECT ST_Union(ST_MakeValid(geom)),"ID" FROM merged GROUP BY "ID" " \
+        $out/${outname} $out/merge.gpkg 
+    rm $out/merge.gpkg
+fi
+
+exit
+
+
+#export BAS=$1
+#export OUT=$2
+### $1 is the raster tile or spatial vector path
+### $2 is the output path
+#
+# if (find "$BAS" -name "*tif") | grep -q $BAS; then 
+#	# merge raster file by first creating a vrt and then do the merge
+#	 gdalbuildvrt -overwrite $BAS/basin.vrt $BAS/*.tif 
+#	 rm -f $BAS/basin.tif 
+#	 gdal_translate -co COMPRESS=DEFLATE -co ZLEVEL=9 $BAS/basin.vrt $OUT/basin.tif
+#	 rm -f $BAS/basin.vrt
+#
+# elif (find "$BAS" -name "*gpkg") | grep -q $BAS; then
+#	 # merge vector file
+#	 rm -f $BAS/basin.gpkg
+#	 ogrmerge.py -single -progress -skipfailures -overwrite_ds -f GPKG -o $BAS/basin.gpkg  $BAS/*.gpkg 
+#	 rm -f $BAS/basin_dissolved.gpkg  
+#	 ogr2ogr  -nlt POLYGON -dialect sqlite -sql "SELECT ST_Union(ST_MakeValid(geom)),"ID" FROM merged GROUP BY "ID" " $OUT/basin_dissolved.gpkg $BAS/basin.gpkg
+#	 rm -f $BAS/basin.gpkg
+# fi
