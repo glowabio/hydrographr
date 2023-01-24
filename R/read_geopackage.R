11322185c916a70bf06e4e5bfea10a411c9a58ab
Sami Domisch <domisch@igb-berlin.de>
2023-01-24 20:15:02 +0100

    Update read_geopackage.R
    Subset possible for network, subc, basin and outlet

--- R/read_geopackage.R
+++ R/read_geopackage.R
@@ -1,8 +1,8 @@
 #' @title Read a GeoPackage file
 #'
-#' @description Reads a GeoPackage vector file from disk either as a table
-#' (data.table), as a directed graph object (igraph), a spatial dataframe (sf)
-#' or a SpatVect object (terra).
+#' @description Reads an entire, or only a subset of a GeoPackage vector file
+#' from disk either as a table #' (data.table), as a directed graph object
+#' (igraph), a spatial dataframe (sf) or a SpatVect object (terra).
 #'
 #' @param gpkg character. Full path of the GeoPackage file.
 #' @param import_as character. "data.table", "graph", "sf", or "SpatVect".
@@ -15,10 +15,14 @@
 #' the GeoPackage. A specific data layer only needs to be defined if the
 #' GeoPackage contains multiple layers. To see the available layers the function
 #' st_layers() from the R package 'sf' can be used. Optional. Default is NULL.
-#' @param subc_id numeric. Vector of the sub-catchment (or stream segment) IDs
-#' in the form of (c(ID1, ID2, ...) for which the spatial objects or attributes
-#' of the GeoPackage should be reported. Currently only implemented for the
-#' stream network GeoPackage file.
+#' @param subc_id numeric. Vector of the sub-catchment (or stream
+#' segment) IDs in the form of (c(ID1, ID2, ...) for which the spatial objects
+#' or attributes of the GeoPackage should be imported. Currently only
+#' implemented for the stream network GeoPackage file. Optional. Default is NULL.
+#' @param name character. The attribute table column name of the stream segment
+#' ("stream"), sub-catchment ("ID"), basin ("ID") or outlet ("ID") column which
+#' is used for subsetting the GeoPackage prior importing. Optional. Defaults
+#' to "stream".
 #' @importFrom DBI dbConnect dbListTables dbGetQuery dbDisconnect
 #' @importFrom RSQLite SQLite
 #' @importFrom data.table setDT
@@ -40,7 +44,7 @@
 #' my_graph <- read_geopackage(gpkg = paste0(my_directory,
 #'                                           "/hydrography90m_test_data",
 #'                                           "/order_vect_59.gpkg"),
-#'                             import_as = "graph")
+#'                                           import_as = "graph")
 #'
 #' # Read the stream network as a data.table
 #' my_dt <- read_geopackage(gpkg = paste0(my_directory,
@@ -57,14 +61,22 @@
 #' my_sf <- read_geopackage(gpkg = paste0(my_directory,
 #'                                        "/hydrography90m_test_data",
 #'                                        "/sub_catchment_59.gpkg"),
-#'                          import_as = "sf",
-#'                          layer_name = "sub_catchment")
+#'                                        import_as = "sf",
+#'                                        layer_name = "sub_catchment")
+#'
+#' # Read a subset of sub-catchments as a SF-object
+#' my_sf <- read_geopackage(gpkg = paste0(my_directory,
+#'                                        "/hydrography90m_test_data",
+#'                                        "/sub_catchment_59.gpkg"),
+#'                                        import_as = "sf",
+#'                                        subc_id = c(513833203, 513833594),
+#'                                        name = "ID")
 #'
 #' # Read the basin as SpatVect object
 #' my_sv <- read_geopackage(gpkg = paste0(my_directory,
 #'                                        "/hydrography90m_test_data",
 #'                                        "/basin_59.gpkg"),
-#'                          import_as = "SpatVect")
+#'                                        import_as = "SpatVect")
 #'
 
 
@@ -72,7 +84,7 @@
 
 # Import a geopackage from disk as a data.table
 read_geopackage <- function(gpkg, import_as = "data.table", layer_name = NULL,
-                            subc_id = NULL) {
+                            subc_id = NULL, name = "stream") {
 
   # Test input arguments
   if (!file.exists(gpkg))
@@ -97,7 +109,7 @@ read_geopackage <- function(gpkg, import_as = "data.table", layer_name = NULL,
 
 
   # Avoid exponential numbers in the table and IDs,
-  # only set this only within the function
+  # set this only within the function
   options(scipen = 999)
 
   if (import_as == "data.table" || import_as == "graph") {
@@ -123,8 +135,9 @@ read_geopackage <- function(gpkg, import_as = "data.table", layer_name = NULL,
                                                    layer_name, "'"))
     } else {
       subc_id2 <- paste(subc_id, collapse = ", ")
-      network_table <- dbGetQuery(conn=conn, statement=paste0("SELECT * FROM ",
-                                "merged", " WHERE stream in (", subc_id2, ")"))
+      network_table <- dbGetQuery(conn=conn, statement=paste0("SELECT * FROM '",
+                                                  layer_name, "' WHERE ", name,
+                                                  " in (", subc_id2, ")"))
     }
 
     # Convert to data.table
@@ -161,8 +174,9 @@ read_geopackage <- function(gpkg, import_as = "data.table", layer_name = NULL,
     sf <- read_sf(gpkg, as_tibble = FALSE)
     } else {
     subc_id2 <- paste(subc_id, collapse = ", ")
-    sf <- read_sf(gpkg, query=paste0("SELECT * FROM ",
-                                "merged", " WHERE stream in (", subc_id2, ")"))
+    sf <- read_sf(gpkg, query=paste0("SELECT * FROM '",
+                                    layer_name, "' WHERE ", name,
+                                    " in (", subc_id2, ")"))
     }
     return(sf)
 
@@ -172,8 +186,9 @@ read_geopackage <- function(gpkg, import_as = "data.table", layer_name = NULL,
     sf <- read_sf(gpkg)
     } else {
     subc_id2 <- paste(subc_id, collapse = ", ")
-    sf <- read_sf(gpkg, query=paste0("SELECT * FROM ",
-                                "merged", " WHERE stream in (", subc_id2, ")"))
+    sf <- read_sf(gpkg, query=paste0("SELECT * FROM '",
+                              layer_name, "' WHERE ", name,
+                              " in (", subc_id2, ")"))
     }
     vect <- vect(sf)
     return(vect)
