f1abf0dd7e5ba6b36aa0f3139d654d5e8f43d557
afrogri37 <afroditi.grigoropoulou@igb-berlin.de>
2023-02-27 19:00:12 +0100

    added bat-to be checked

--- inst/bat/get_distance.bat
+++ inst/bat/get_distance.bat
@@ -1,10 +1,10 @@
 @echo on
 
-set WSLENV=%1 %2 %3 %4 %5 %6 %7 %8 %9
-set DATA=%1
+set WSLENV=%1 %2 %3 %4 %5 %6 %7 %8 %9 %10 %11 %12
+set DATA=%2
 shift
-set SHDIR=%9
+set SHDIR=%12
 
 wsl dos2unix %DATA%
-wsl dos2unix %SHDIR% 
+wsl dos2unix %SHDIR%
 wsl bash  %SHDIR% $WSLENV
