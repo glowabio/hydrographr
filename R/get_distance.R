82e55fa05698276d9036b7207befeb70f4104e45
afrogri37 <afroditi.grigoropoulou@igb-berlin.de>
2023-02-28 10:42:41 +0100

    edited examples and cleaned

--- R/get_distance.R
+++ R/get_distance.R
@@ -13,7 +13,6 @@
 #' If NULL and distance is set to 'network' or 'both', the basin IDs will be
 #' extracted automatically. Default is NULL.
 #' @param basin_layer character. Full path to the basin ID .tif layer.
-#' @param subc_layer character. Full path to the sub-catchment ID .tif layer.
 #' @param stream_layer character. Full path of the stream network .gpkg file.
 #' @param distance character. One of "euclidean", "network", or "both".
 #' If "euclidean", the euclidean distances between all pairs of points are
@@ -36,7 +35,15 @@
 #' @details
 #'...
 #'
-#' @author Jaime Garcia Marquez, Afroditi Grigoropoulou, Maria M. Üblacker
+#' @returns
+#' If distance='euclidean', a distance matrix of the euclidean distances between
+#' all the pairs of points (object of class data.frame). If distance='network',
+#' a data.frame with three columns: from_id, to_id, dist. The 'dist' column
+#' includes the distance of the shortest path along the network from the node
+#' from_id to the node to_id. If distance='both', a list containing both objects
+#' is returned.
+#'
+#' @author Afroditi Grigoropoulou, Maria M. Üblacker, Jaime Garcia Marquez
 #'
 #' @references
 #' \url{https://grass.osgeo.org/grass82/manuals/v.net.allpairs.html}
@@ -55,46 +62,25 @@
 #' download_test_data(my_directory)
 #'
 #' # Load occurrence data
-#' species_occurence <- read.table(paste0(my_directory,
+#' species_occurrence <- read.table(paste0(my_directory,
 #'                             "/hydrography90m_test_data/spdata_1264942.txt"),
 #'                               header = TRUE)
-# Define full path to the basin and sub-catchments raster layer
+#'
+#  # Define full path to the basin raster layer
 #' basin_rast <- paste0(my_directory,
 #'                      "/hydrography90m_test_data/basin_1264942.tif")
-#' subc_rast <- paste0(my_directory,
-#'                     "/hydrography90m_test_data/subcatchment_1264942.tif")
+#'
+#' # Define full path to the sub-catchment raster layer
+#' basin_rast <- paste0(my_directory,
+#'                      "/hydrography90m_test_data/subcatchment_1264942.tif")
 #'
 #' # Define full path to the vector file of the stream network
 #' stream_vect <- paste0(my_directory,
 #'                       "/hydrography90m_test_data/order_vect_59.gpkg")
 #'
-# EITHER
-# Extract basin and sub-catchment IDs from the Hydrography90m layers beforehand
-#' hydrography90m_ids <- extract_ids(data = species_occurence,
-#'                                   lon = "longitude",
-#'                                   lat = "latitude",
-#'                                   id = "occurrence_id",
-#'                                   subc_layer = subc_rast,
-#'                                   basin_layer = basin_rast)
-#'
-#' # Snap data points to the stream segment of the provided sub-catchment ID
-#' snapped_coordinates <- snap_to_subc_segment(data = hydrography90m_ids,
-#'                                             lon = "longitude",
-#'                                             lat = "latitude",
-#'                                             id = "occurrence_id",
-#'                                             basin_id = "basin_id",
-#'                                             subc_id = "subcatchment_id",
-#'                                             basin_layer = basin_rast,
-#'                                             subc_layer = subc_rast,
-#'                                             stream_layer = stream_vect,
-#'                                             n_cores = 2)
-#' # Show head of output table
-#' head(snapped_coordinates)
-#'
-#' # OR
 #' # Automatically extract the basin and sub-catchment IDs and
 #' # snap the data points to the stream segment
-#' snapped_coordinates <- snap_to_subc_segment(data = species_occurence,
+#' snapped_coordinates <- snap_to_subc_segment(data = species_occurrence,
 #'                                             lon = "longitude",
 #'                                             lat = "latitude",
 #'                                             id = "occurrence_id",
@@ -105,17 +91,27 @@
 #' # Show head of output table
 #' head(snapped_coordinates)
 #'
+#' # Get the euclidean distance and the distance along the network between all
+#' # pairs of points
+#' distance_table <- get_distance(data = snapped_coordinates,
+#'                                lon = "lon_snap",
+#'                                lat = "lat_snap",
+#'                                id = "occurrence_id",
+#'                                basin_id = "basin_id",
+#'                                basin_layer = basin_rast,
+#'                                stream_layer = stream_vect,
+#'                                distance = "network",
+#'                                n_cores = 1,
+#'                                quiet = FALSE)
 
 
-get_distance <- function(data, lon, lat, id, basin_id = NULL, subc_id = NULL,
-                         basin_layer = NULL, subc_layer = NULL,
-                         stream_layer = NULL, distance = "all",
-                         n_cores = 1, quiet = TRUE) {
+get_distance <- function(data, lon, lat, id, basin_id = NULL,
+                         basin_layer = NULL, stream_layer = NULL,
+                         distance = "all", n_cores = 1, quiet = TRUE) {
 
 
   # Check if any of the arguments is missing
-  for (arg in  c(data, lon, lat, id, basin_layer, subc_layer,
-                stream_layer)) {
+  for (arg in  c(data, lon, lat, id, basin_layer, stream_layer)) {
     if (missing(arg))
       stop(paste0(quote(arg), " is missing."))
     }
@@ -125,7 +121,7 @@ get_distance <- function(data, lon, lat, id, basin_id = NULL, subc_id = NULL,
   if (!is(data, "data.frame"))
     stop("data: Has to be of class 'data.frame'.")
 
-  # Check if lon, lat, id, basin_id, and subc_id column names
+  # Check if lon, lat, id, basin_id column names
   # are character strings
   for (name in  c(lon, lat, id)) {
   if (!is.character(name))
@@ -136,11 +132,8 @@ get_distance <- function(data, lon, lat, id, basin_id = NULL, subc_id = NULL,
     if (!is.character(basin_id))
       stop(paste0("Column name ", basin_id, " is not a character string."))
 
-  if (!is.null(subc_id))
-    if (!is.character(subc_id))
-      stop(paste0("Column name ", subc_id, " is not a character string."))
 
-  # Check if lon, lat, id, basin_id, and subc_id column names exist
+  # Check if lon, lat, id, basin_id column names exist
   for (name in c(lon, lat, id)) {
     if (is.null(data[[name]]))
       stop(paste0("Column name '", name, "' does not exist."))
@@ -150,9 +143,6 @@ get_distance <- function(data, lon, lat, id, basin_id = NULL, subc_id = NULL,
     if (is.null(data[[basin_id]]))
       stop(paste0("Column name '", basin_id, "' does not exist."))
 
-  if (!is.null(subc_id))
-    if (is.null(data[[subc_id]]))
-      stop(paste0("Column name '", subc_id, "' does not exist."))
 
   # Check id for duplicated IDs
   if (length(unique(data[[id]])) != length(data[[id]]))
@@ -163,18 +153,18 @@ get_distance <- function(data, lon, lat, id, basin_id = NULL, subc_id = NULL,
     stop("distance: Has to be 'euclidean', 'network', or 'both'.")
 
    # Check if paths exists
-  for (path in c(basin_layer, subc_layer, stream_layer)) {
+  for (path in c(basin_layer, stream_layer)) {
    if (!file.exists(path))
      stop(paste0("File path: ", path, " does not exist."))
   }
 
-  # Check if basin_layer and subc_layer ends with .tif
-  for (path in c(basin_layer, subc_layer)) {
+  # Check if basin_layer ends with .tif
+  for (path in basin_layer) {
     if (!endsWith(path, ".tif"))
       stop(paste0("File path: ", path, " does not end with .tif"))
   }
 
-  # Check if basin_layer and subc_layer ends with .gpkg
+  # Check if basin_layer ends with .gpkg
   if (!is.null(stream_layer))
     if (!endsWith(stream_layer, ".gpkg"))
       stop(paste0("File path: ", stream_layer, " does not end with .gpkg"))
@@ -197,8 +187,9 @@ get_distance <- function(data, lon, lat, id, basin_id = NULL, subc_id = NULL,
   if (is.null(basin_id)) {
     # Extract basin ids
     basin_ids <- extract_ids(data = data, lon = lon, lat = lat,
-                             subc_layer = NULL,
-                             basin_layer = basin_layer, quiet = quiet)
+                             subc_layer = NULL,basin_layer = basin_layer,
+                             quiet = quiet)
+
     # Join with data and select columns needed for the bash script
     # Note: "id" should be the first column
     ids <- left_join(data, basin_ids, by = c(lon, lat)) %>%
@@ -230,7 +221,7 @@ get_distance <- function(data, lon, lat, id, basin_id = NULL, subc_id = NULL,
     dist_eucl_tmp_path <- paste0(tempdir(),
                                  "/distance/dist_eucl/dist_euclidean_",
                                  rand_string, ".csv")
-  }
+  } else {dist_eucl_tmp_path <- NULL}
 
   if(distance == "network" || distance == "both") {
     dir.create(paste0(tempdir(), "/distance/dist_net"), showWarnings = FALSE)
@@ -239,7 +230,7 @@ get_distance <- function(data, lon, lat, id, basin_id = NULL, subc_id = NULL,
                                 "/distance/dist_net/dist_network_",
                                 rand_string, ".csv")
 
-  }
+  } else {dist_net_tmp_path <- NULL}
 
 
   # Convert NULL argument to "NA" so that the bash script can evaluate
@@ -312,7 +303,6 @@ get_distance <- function(data, lon, lat, id, basin_id = NULL, subc_id = NULL,
   if(distance == "both") {
     return(list(dist_eucl_table, dist_net_table))
   }
-
 
 
   # Remove files in the tmp folder
