c3689ab64779cf74c717a1dc4cc10c08ddf8c1c7
afrogri37 <afroditi.grigoropoulou@igb-berlin.de>
2023-04-25 16:22:14 +0200

    edit description

--- R/update_wsl.R
+++ R/update_wsl.R
@@ -1,7 +1,7 @@
 #' @title Update the Windows Subsystem for Linux
 #'
-#' @description The "sudo apt-get update" command updates the package lists from the
-#' repositories to ensure that the system has the latest information about
+#' @description The "sudo apt-get update" command updates the package lists from
+#' the repositories to ensure that the system has the latest information about
 #' available software packages.
 #' The "sudo apt-get upgrade" command upgrades the installed packages to their
 #' latest available versions by downloading and installing any updates that are
@@ -11,22 +11,18 @@
 #' @param data a data.frame or data.table with lat/lon coordinates in WGS84.
 #' @param quiet logical. If FALSE, the standard output will be printed.
 #'
-#' @importFrom stringi stri_rand_strings
 #' @importFrom processx run
 #' @export
 #'
 #' @note
-#'#' Sometimes VPN connections block the downloading of necessary updates. We thus
-#' recommend to run this function outside any VPN connection.
+#' Sometimes VPN connections block the downloading of necessary updates.
+#' We thus recommend to run this function outside any VPN connection.
 #'
 #' @author Afroditi Grigoropoulou
 #'
 #' @references
 #' \url{https://manpages.debian.org/stretch/apt/apt.8.en.html}
 #'
-
-
-
 
 update_wsl <- function(password) {
 
