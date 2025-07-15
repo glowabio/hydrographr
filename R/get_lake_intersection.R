95ee74d336be3012c7f89c1f72f1650287373851
tomitho <113701037+tomitho@users.noreply.github.com>
2025-07-15 16:27:32 +0200

    cleaned get_lake_intersection function
    Cleaned get_lake_intersection function from unecessary comments and unused lines of code.

--- R/get_lake_intersection.R
+++ R/get_lake_intersection.R
@@ -1,11 +1,12 @@
 #' @title Get intersection points between Hydrography90m stream network and lake shapefiles
 #'
-#' @description Add describtion text here
-#'
-#'  Any thing the user needs to notice? add Note
+#' @description The function identifies, for each lake ID, the intersection points
+#' between the stream network (i.e., Hydrography90m) and the corresponding
+#' lake's geospatial file (e.g., HydroLAKES shapefile).
 #'
 #' @param data a data.frame or data.table that contains the columns regarding
-#' the lake ids, i.e. HydroLAKES ids.
+#' the lake ids, i.e. HydroLAKES ids. (i.e., output of extract_lake_ids);
+#' see also help(extract_lake_ids)
 #' @param lake_id character. The name of the column containing lake ids.
 #' @param lakes character. Full path to lake geo-spatial files; i.e. HydroLAKES shapefiles.
 #' @param lake_name character. The name of the attribute table column in
@@ -18,16 +19,13 @@
 #' @param edge character. Full path to GuidosToolbox Workbench MSPA tool
 #' see also (https://forest.jrc.ec.europa.eu/en/activities/lpa/gtb/)
 #' (i.e., output of get_predict_table); see also help(get_predict_table)
-#' @param stream character. Full path to Hydrography 90m stream network tif file
+#' @param stream character. Full path to Hydrography90m stream network tif file
 #' add a reference were to check the necessary tif files for lake analysis
-#' @param flow character. Full path to Hydrography 90m flow accumulation tif file
-#' @param glbasins character. Full path to Hydrography 90m basin tif file
+#' @param flow character. Full path to Hydrography90m flow accumulation tif file
+#' @param basins character. Full path to Hydrography90m basin tif file
 #' @param lake_dat character. Full path of the output.csv table,
 #' i.e., the lake intersection table.
-#' @param n_cores interger. Number of cores used in parallelzation, Default is one.
-#' @param read logical. If TRUE, then the model .csv table
-#' gets read into R as data.table and data.frame.
-#' if FALSE, the table is only stored on disk. Default is FALSE.
+#' @param n_cores interger. Number of cores used in parallelization, Default is one.
 #' @param quiet logical. If FALSE, the standard output will be printed.
 #' Default is TRUE.
 #'
@@ -39,10 +37,10 @@
 #' @author Jaime Garcia Marquez, Thomas Tomiczek
 #'
 #' @references
-#' add reference manual html here
 #' https://grass.osgeo.org/grass82/manuals/
-#' add manual of the MSPA analysis tool here
-#'
+#' Soille P. and Vogt P. (2009). Morphological segmentation of binary patterns.
+#' Pattern Recognition Letters 30, 4:456-459, doi: 1.1016/j.patrec.2008.10.015
+#' \url{https://ies-ows.jrc.ec.europa.eu/gtb/GTB/MSPA_Guide.pdf}
 #'
 #' @examples
 #' # Download test data into the temporary R folder
@@ -50,45 +48,49 @@
 #' my_directory <- tempdir()
 #' download_test_data(my_directory)
 #'
-#' # Get the lake intersection table for all lakes in the test data
 #' # Note: For this we need the lake IDs (i.e. HydroLake ID), extracted from the
 #' # extract_lake_ids.R function; see also help(extract_lake_ids)
 #'
+#' data <- fread(paste0(my_directory,
+#'                          "/hydrography90m_test_data",
+#'                          "/lake_id.txt"),
+#'                          header = TRUE)
+#' lake_id <- "lake_id"
 #'
-# data <- fread(paste0(my_directory,
-#                         "/hydrography90m_test_data",
-#                         "/lake_id.txt"),
-#                         header = TRUE)
-# lake_id <- "lake_id"
-# lakes <- (paste0(my_directory,
-#                      "/hydrography90m_test_data",
-#                      "/lakes_corsica.shp"))
-# # To run the function we need to have installed GuidosToolbox Workbench MSPA tool
-# # see also (https://forest.jrc.ec.europa.eu/en/activities/lpa/gtb/) and (cite lake vignette)
-# edge <- (paste0(my_directory,
-#                      "/GWB"))
-# stream <- (paste0(my_directory,
-#                      "/hydrography90m_test_data",
-#                      "/stream_1264942.tif"))
-# flow <- (paste0(my_directory,
-#                      "/hydrography90m_test_data",
-#                      "/flow_1264942.tif"))
-#
-# glbasins <- (paste0(my_directory,
-#                      "/hydrography90m_test_data",
-#                      "/basin_1264942.tif"))
-#
-# lake_dat <- (paste0(my_directory,
-#                      "/hydrography90m_test_data"))
-#
-# lake_intersect_table <- get_lake_intersection(data, lake_id = "lake_id",
-#                                   lakes, lake_name = "lake_id", buffer = TRUE, edge,
-#                                   stream, flow, glbasins, lake_dat, n_cores = 1, quiet = FALSE)
+#' lakes <- (paste0(my_directory,
+#'                     "/hydrography90m_test_data",
+#'                     "/lakes_corsica.shp"))
+#'
+#' # To run the function we need to have installed GuidosToolbox Workbench MSPA tool
+#' # see also (https://forest.jrc.ec.europa.eu/en/activities/lpa/gtb/) and
+#' (https://glowabio.github.io/hydrographr/articles/case_study_lake_workflow.html)
+#' edge <- (paste0(my_directory,
+#'                     "/GWB"))
+#'
+#' stream <- (paste0(my_directory,
+#'                     "/hydrography90m_test_data",
+#'                     "/stream_1264942.tif"))
+#'
+#' flow <- (paste0(my_directory,
+#'                     "/hydrography90m_test_data",
+#'                     "/flow_1264942.tif"))
+#'
+#' basins <- (paste0(my_directory,
+#'                     "/hydrography90m_test_data",
+#'                     "/basin_1264942.tif"))
+#'
+#' lake_dat <- (paste0(my_directory,
+#'                     "/hydrography90m_test_data"))
+#'
+#' lake_intersect_table <- get_lake_intersection(data, lake_id = "lake_id",
+#'                             lakes, lake_name = "lake_id", buffer = TRUE, edge,
+#'                             stream, flow, basins, lake_dat, n_cores = 1,
+#'                             quiet = FALSE)
 
 
 get_lake_intersection <- function(data, lake_id = "HydroLAKES_polys_v10", lakes,
                                   lake_name, buffer = TRUE, edge, stream, flow,
-                                  glbasins, lake_dat,
+                                  basins, lake_dat,
                                   n_cores = 1, quiet = TRUE) {
 
   # Check if input data is of type data.frame,
@@ -99,7 +101,6 @@ get_lake_intersection <- function(data, lake_id = "HydroLAKES_polys_v10", lakes,
   # Check if paths exists
   if (!file.exists(lakes))
     stop(paste0("Please provide the path to lake geo-spatial files"))
-
 
   # Check if paths exists
   if (is.null(edge))
@@ -114,7 +115,7 @@ get_lake_intersection <- function(data, lake_id = "HydroLAKES_polys_v10", lakes,
     stop(paste0("Please provide the path to the flow raster file"))
 
   # Check if paths exists
-  if (!file.exists(glbasins))
+  if (!file.exists(basins))
     stop(paste0("Please provide the path to the basin raster file"))
 
   # Check if paths exists
@@ -125,6 +126,7 @@ get_lake_intersection <- function(data, lake_id = "HydroLAKES_polys_v10", lakes,
   if (!is.logical(quiet))
     stop("quiet: Has to be TRUE or FALSE.")
 
+  # create file required by GuidosToolbox Workbench MSPA tool
 mspa_content <- ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; GWB_MSPA parameter file:
 ;; NOTE: do NOT delete or add any lines in this parameter file!
@@ -159,8 +161,7 @@ mspa_content <- ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 0
 ****************************************************************************"
 
-writeLines(mspa_content, paste0(tempdir(), "/mspa-parameters.txt"))
-# writeLines(mspa_content, paste0(edge,"input/", "mspa-parameters.txt"))
+  writeLines(mspa_content, paste0(tempdir(), "/mspa-parameters.txt"))
 
   values <- rep(0, 256) # Create a vector of 256 zeros
 
@@ -177,15 +178,11 @@ writeLines(mspa_content, paste0(tempdir(), "/mspa-parameters.txt"))
   fwrite(result, paste0(tempdir(), "/mspa_reclass_code.txt"), col.names = FALSE,
          row.names = FALSE, quote = FALSE, sep = " ")
 
-  # Print the resulting data.frame
-  # print(result)
 
-  # write a line of code to transform number in meters to units in degree
+  # write a line of code to transform meters to units in degree
   if (is.numeric(buffer) && length(buffer) > 0) {
     buffer <- (buffer * 0.000009)
   }
-
-  ### be sure to convert csv files into txt files for the sh script to work! ###
 
   # Create random string
   rand_string <- stri_rand_strings(n = 1, length = 8, pattern = "[A-Za-z0-9]")
@@ -197,7 +194,8 @@ writeLines(mspa_content, paste0(tempdir(), "/mspa-parameters.txt"))
 
   # Path for tmp ids.txt file
   ids_tmp_path <- paste0(tempdir(), "/ids_", rand_string, ".txt")
-  ## write to tempory file to convert to txt file
+
+  # write to tempory file to convert to txt file
   fwrite(id_dat, ids_tmp_path, col.names = TRUE,
          row.names = FALSE, quote = FALSE, sep = " ")
 
@@ -212,7 +210,7 @@ writeLines(mspa_content, paste0(tempdir(), "/mspa-parameters.txt"))
     # Call the external .sh script extract_ids() containing the gdal function
     processx::run(system.file("sh", "get_lake_intersection.sh", package = "hydrographr"),
                   args = c(ids_tmp_path, lake_id, lakes, lake_name, buffer,
-                           edge, stream, flow, glbasins,
+                           edge, stream, flow, basins,
                            tempdir(), lake_dat, n_cores),
                   echo = !quiet)
 
@@ -222,34 +220,25 @@ writeLines(mspa_content, paste0(tempdir(), "/mspa-parameters.txt"))
     # Change path for WSL
     wsl_ids_tmp_path <- fix_path(ids_tmp_path)
     wsl_lakes_path <- fix_path(lakes)
-  # wsl_edge_path <- fix_path(edge)
     wsl_stream_path <- fix_path(stream)
     wsl_flow_path <- fix_path(flow)
-    wsl_glbasins_path <- fix_path(glbasins)
+    wsl_basins_path <- fix_path(basins)
     wsl_tmp_path <- fix_path(tempdir())
     wsl_lake_dat_path <- fix_path(lake_dat)
     wsl_sh_file <- fix_path(
-      system.file("sh", "get_lake_intersection.sh",
-                  package = "hydrographr"))
+                            system.file("sh", "get_lake_intersection.sh",
+                                        package = "hydrographr"))
 
     processx::run(system.file("bat", "get_lake_intersection.bat",
                               package = "hydrographr"),
                   args = c(wsl_ids_tmp_path, lake_id, wsl_lakes_path, lake_name,
                            buffer, edge, wsl_stream_path, wsl_flow_path,
-                           wsl_glbasins_path, wsl_tmp_path, wsl_lake_dat_path,
+                           wsl_basins_path, wsl_tmp_path, wsl_lake_dat_path,
                            n_cores, wsl_sh_file, echo = !quiet))
 
   }
-  # Read in the file containing the ids setting fill=TRUE, for the case that
-  # some coordinates were in null cells so they did not get an ID
-  data_ids <- fread(paste0(tempdir(),  "/ids_", rand_string, ".txt"),
-                    keepLeadingZeros = TRUE, header = TRUE, sep = " ",
-                    fill = TRUE)
+  # Return message
+  print(paste0("Lake intersection tables are stored under", lake_dat))
 
-  # Remove all files in the tmp folder
-  file.remove(ids_tmp_path)
-
-  # Return data frame
-  return(data_ids)
 
 }
--- inst/sh/get_lake_intersection.sh
+++ inst/sh/get_lake_intersection.sh
@@ -1,71 +1,45 @@
 #!/bin/sh
 
-# path to hydrolake shapefiles export LAKE_SHAPE=/mnt/shared/data_from_yale/dataproces/HydroLAKES/HydroLAKES_polys_v10_shp/HydroLAKES_polys_v10.shp
-# path to global file of basins export GLBASINS=/mnt/shared/data_from_yale/MERIT_HYDRO/lbasin_tiles_final20d_ovr/all_lbasin.tif
-# path to global basin of computational units export GLCOMPUNITS=/mnt/shared/data_from_yale/MERIT_HYDRO/lbasin_compUnit_overview/lbasin_compUnit.tif
-# path to basins in each computational unit export CUBASINS=/mnt/shared/data_from_yale/MERIT_HYDRO/CompUnit_lbasin
-# path to flow.tif file export FLOW=/mnt/shared/data_from_yale/MERIT_HYDRO/flow_tiles/all_tif_dis.vrt
-# path to stream.tif file export STREAM=/mnt/shared/data_from_yale/MERIT_HYDRO/stream_tiles_final20d_1p/all_stream_dis.vrt
-# SHP_NAME="HydroLAKES_polys_v10"
-# path to GWB analysis tool export GWB=/data/ttomiczek/GWB/
-
 export DATA=$1
-export VAR_ID=$2 # maybe dont need this variable when in the R script every data table is rearranged that ID column is the first column??
+export VAR_ID=$2
 export LAKE_SHAPE=$3
 export SHP_NAME=$4
 export BUF=$5
 export GWB=$6 # full path to GWB MSPA analysis tool
 export STREAM=$7 # full path to stream.tif file
 export FLOW=$8 # full path to flow.tif file
-export GLBASINS=$9 # full path to global file of basins
+export GLBASINS=$9 # full path to basins.tif file
 export TMPDIR=${10}
 export OUTDIR=${11}
 export NCORES=${12}
 
-
-# if gpkg geo data base do get SHP_NAME for HydroLAKES SHP_NAME is HydroLAKES_polys_v10
-# ogrinfo $LAKE_SHAPE -so
-# if input is a shape file do get  no does not support dataformat use ogrinfo
-# gdalinfo $LAKE_SHAPE
-
-# read Hydrolakes ids into an array to process the script in parallel
-# LK=($(awk '{print $1}' $DATA))
-# get array without the first line of the header otherwise alter the data table in R before sending it to the sh script
-# LK=($(awk 'NR > 1 && $1 ~ /^[0-9]+([.][0-9]+)?$/' "$DATA"))
-
-# export LK=$LK
-# echo $LK
 
 get_lake_intersection() {
 export LK=$1
 
 SECONDS=0
 echo "LAKE $LK is being processed"
-# SHP_NAME="HydroLAKES_polys_v10"
-# try this to run in parallel
-# parallel ogr2ogr $TMPDIR/lake_{1}.shp $LAKE_SHAPE -sql "SELECT * FROM $SHP_NAME WHERE $VAR_ID = {1}" ::: "${LK[@]}"
-# if gpkg file use the following
+
 ogr2ogr $TMPDIR/lake_${LK}.shp \
 	     $LAKE_SHAPE \
        -sql "SELECT * FROM $SHP_NAME WHERE $VAR_ID = ${LK}"
 
-# if its a shapefile use the following
 ogr2ogr -where "$VAR_ID = $LK" $TMPDIR/lake_${LK}.shp $LAKE_SHAPE
-# ogr2ogr -f "ESRI Shapefile" -sql "SELECT * FROM $SHP_NAME WHERE $VAR_ID = ${LK}" $TMPDIR/lake_${LK}.shp $LAKE_SHAPE
-# this name of the shape file HydroLAKES_polys_v10 needs to be defined before either by the user or automatically
-        export LAKE=$TMPDIR/lake_${LK}.shp
 
-    ##  calculate the area to define the buffer distance
+export LAKE=$TMPDIR/lake_${LK}.shp
 
-    # check basename of polygon file
-    export pn=$(basename $LAKE .shp)
+##  calculate the area to define the buffer distance
 
-    area=$(ogrinfo -dialect SQLite -sql \
-        "SELECT (ST_Area(ST_Transform(Geometry,3395)))/1000000 FROM ${pn}" \
-        $LAKE | awk '/=/{print $4}')
- if [ "$BUF" = "TRUE" ]
+# check basename of polygon file
+export pn=$(basename $LAKE .shp)
+
+area=$(ogrinfo -dialect SQLite -sql \
+    "SELECT (ST_Area(ST_Transform(Geometry,3395)))/1000000 FROM ${pn}" \
+     $LAKE | awk '/=/{print $4}')
+
+  if [ "$BUF" = "TRUE" ]
   then
- # rule to define buffer distance; bd= units are map units in degree (0.0054 = 600 meters; 0.0009 = 100 meters)
+    # rule to define buffer distance; bd= units are map units in degree (0.0054 = 600 meters; 0.0009 = 100 meters)
     if (( $(echo "${area} <= 1" | bc -l) )); then export bd=0.0009; fi
     if (( $(echo "${area} > 1" | bc -l) && $(echo "${area} <= 50" | bc -l) )); then export bd=0.0018; fi
     if (( $(echo "${area} > 50" | bc -l) && $(echo "${area} <= 100" | bc -l) )); then export bd=0.0027; fi
@@ -94,7 +68,6 @@ ogr2ogr -where "$VAR_ID = $LK" $TMPDIR/lake_${LK}.shp $LAKE_SHAPE
    export LAKE=$TMPDIR/lake_${LK}.shp
  fi
 
-# export LAKE=$TMPDIR/buffer_${LK}.shp
 export pn=$(basename $LAKE .shp)
 
 EXTENSION=($( ogrinfo  $LAKE -so -al | grep Extent \
@@ -144,13 +117,6 @@ gdal_rasterize -a_srs EPSG:4326  -at -a diss -l $pn \
 export TMPDIR=$TMPDIR
 export LK=$LK
 
-# grass -f --text --tmp-location $TMPDIR/lake_${LK}cp.tif <<EOF
-#     r.external -o input=$TMPDIR/lake_${LK}cp.tif  output=out
-#     g.region zoom=out
-#     r.out.gdal -cm input=out out=$TMPDIR/lake_${LK}rm.tif \
-#     format=GTiff type=Byte createopt="COMPRESS=DEFLATE" nodata=0 --overwrite
-# EOF
-# Maybe try out this code if lake_{$LK}cp.tif does not exist
 grass --tmp-project $TMPDIR/lake_${LK}cp.tif --exec bash -c "
     r.external -o input=$TMPDIR/lake_${LK}cp.tif output=out &&
     g.region zoom=out &&
@@ -174,58 +140,22 @@ gdal_translate \
     -projwin $( pkinfo -i $TMPDIR/lake_${LK}.tif -bb | grep -Eo '[+-]?[0-9]+([.][0-9]+)?' ) \
     -co COMPRESS=LZW -co ZLEVEL=9 $GLBASINS $TMPDIR/extentLB_${LK}.tif
 
-### how does the user get the $GLBASINS .tif file? download it before through download function croping allbasin.tif to the extent of interest? ###
-
-# crop compunit raster file to the lake extention
-# gdal_translate \
-  #  -projwin $( pkinfo -i $TMPDIR/lake_${LK}.tif -bb | grep -Eo '[+-]?[0-9]+([.][0-9]+)?' ) \
-  # -co COMPRESS=LZW -co ZLEVEL=9 $GLCOMPUNITS $TMPDIR/extentCU_${LK}.tif
-
-### how does the user get the $GLCOMPUNITS .tif file? download it before through download function? ###
-
-# mask the basins raster to the lake boundaries to identify the basin IDs
-# that fully overlap with lake
 pksetmask -i $TMPDIR/extentLB_${LK}.tif \
     -m $TMPDIR/lake_${LK}.tif -msknodata 0 -nodata 0 \
     -o $TMPDIR/maskLB_${LK}.tif -co COMPRESS=LZW -co ZLEVEL=9
-
-### should there be an option to save lake.tif file to disk for the user? ###
 
 # identify basin IDs
 echo "$LK $(pkstat -i $TMPDIR/maskLB_${LK}.tif -hist \
     | awk '$2 > 0 && $1 > 0 {print $1}' | sed -z 's/\n/ /g')" \
     > $TMPDIR/ref_lbasinIDs_${LK}.txt
 
-# mask the compunit raster to the lake boundaries to identify the compUnit IDs
-# that fully overlap with lake
-# pksetmask -i $TMPDIR/extentCU_${LK}.tif \
-  #  -m $TMPDIR/lake_${LK}.tif -msknodata 0 -nodata 0 \
-  # -o $TMPDIR/maskCU_${LK}.tif -co COMPRESS=LZW -co ZLEVEL=9
-
-# identify CompUnit IDs
-# echo "$LK $(pkstat -i $TMPDIR/maskCU_${LK}.tif -hist \
-  #  | awk '$2 > 0 && $1 > 0 {print $1}' | sed -z 's/\n/ /g')" \
-  # > $TMPDIR/ref_CompUnitIDs_${LK}.txt
-
-# remove tmp files
-# rm $TMPDIR/maskCU_${LK}.tif $TMPDIR/maskLB_${LK}.tif \
-# $TMPDIR/extentCU_${LK}.tif $TMPDIR/extentLB_${LK}.tif \
-# $TMPDIR/lake_${LK}rm.tif $TMPDIR/lake_${LK}cp.tif
 
     # get the IDs of the COmpUnits
     CUID=($(cut -d" " -f2- $TMPDIR/ref_lbasinIDs_${LK}.txt))
 
-    # Join Computational Units as VRT
-    # create file with list of files to join
-    # for i in ${CUID[@]}; do find $GLBASINS -name "lbasin_${i}_msk.tif"; done \
-      #  > $TMPDIR/my_list_${LK}.txt
-    # create the vrt
-    # gdalbuildvrt $TMPDIR/CompUnits_${LK}.vrt \
-      # -input_file_list $TMPDIR/my_list_${LK}.txt
-
     rm $TMPDIR/my_list_${LK}.txt
 
-    # make list of lbasins IDs as an arrray
+    # make list of basins IDs as an arrray
     export  MBID=( $(cut -d" " -f2- $TMPDIR/ref_lbasinIDs_${LK}.txt) )
 
    # reclassify raster with lbasins of interest (1) else is (0)
@@ -241,23 +171,12 @@ echo "$LK $(pkstat -i $TMPDIR/maskLB_${LK}.tif -hist \
         <(printf "%s\n" ${col2[@]:1:${#col2[@]}}) \
         > $TMPDIR/reclass_code_${LK}.txt
 
-    # run reclassification
-    pkreclass -i $GLBASINS \
+# run reclassification
+pkreclass -i $GLBASINS \
         -o $TMPDIR/lbasin_reclass_${LK}.vrt \
         --code $TMPDIR/reclass_code_${LK}.txt
 
-    rm $TMPDIR/reclass_code_${LK}.txt
-
-# export LK=$LK
-
-# grass -f --text --tmp-location $TMPDIR/lbasin_reclass_${LK}.vrt <<EOF_SCRIPT
-#     r.external -o input=$TMPDIR/lbasin_reclass_${LK}.vrt output=out
-#     g.region zoom=out
-#     g.region -p
-#     r.out.gdal input=out out=${TMPDIR}/ALLbasins_${LK}.tif format=GTiff type=Byte createopt="COMPRESS=DEFLATE,BIGTIFF=YES" --overwrite
-#     region_info=\$(g.region -w | awk -F '[=,]' '{print \$2,\$3,\$4,\$5}')
-#     echo "\$LK \$region_info" > $TMPDIR/lakes_ref_extent_${LK}.txt
-# EOF_SCRIPT
+rm $TMPDIR/reclass_code_${LK}.txt
 
 grass --tmp-project $TMPDIR/lbasin_reclass_${LK}.vrt --exec bash -c "
     r.external -o input=$TMPDIR/lbasin_reclass_${LK}.vrt output=out &&
@@ -267,15 +186,10 @@ grass --tmp-project $TMPDIR/lbasin_reclass_${LK}.vrt --exec bash -c "
         format=GTiff type=Byte createopt='COMPRESS=DEFLATE,BIGTIFF=YES' --overwrite &&
     region_info=\$(g.region -w | awk -F '[=,]' '{print \$2,\$3,\$4,\$5}') &&
     echo \"${LK} \$region_info\" > $TMPDIR/lakes_ref_extent_${LK}.txt
-"
+    "
 
 # extention to consider for upstream creation
 export EXT=$(awk '{print $2, $3, $4, $5}' $TMPDIR/lakes_ref_extent_${LK}.txt)
-
-### add an option to use raster files here instead of lake shape files ###
-# tell user to have under $GWB the folder $GWB/input and $GWB/output
-
-# mkdir -p $GWB/{in,out}put # check if sudo mkdir is necessary?
 
 pkreclass -co COMPRESS=LZW -co ZLEVEL=9 -ot Byte -of GTiff \
         -i $TMPDIR/lake_${LK}.tif \
@@ -286,16 +200,13 @@ echo "START MSPA"
 # make sure $GWB/output is empty to work
 rm -rf $GWB/output/*
 cp $TMPDIR/lakes_ref_extent_${LK}.txt $OUTDIR/lakes_ref_extent_${LK}.txt
-# create mspa-parameters.txt before in R (check if its always the same structure)
-cp $TMPDIR/mspa-parameters.txt "$GWB/input" # this file needs to be created in R!
-# cat $GWB/input/mspa-parameters.txt
+cp $TMPDIR/mspa-parameters.txt "$GWB/input"
+
 GWB_MSPA -i=$GWB/input -o=$GWB/output
 cp $GWB/output/lake_${LK}_mspa/lake_${LK}_8_1_0_1.tif $TMPDIR
 rm $GWB/output/mspa.log
 
 echo "MSPA is finished"
-
-# rm -rf $DIR/GWB/{in,out}put # if these folders were created wiht mkdir then remove them afterwards
 
 ### reclassification of MSPA output to retain only the outer borders
 pkreclass -co COMPRESS=LZW -co ZLEVEL=9 \
@@ -322,27 +233,6 @@ gdal_translate -co COMPRESS=LZW -co ZLEVEL=9 \
     -projwin $( pkinfo -i $TMPDIR/mspa_${LK}_l.tif -bb | grep  -Eo '[+-]?[0-9]+([.][0-9]+)?' ) \
     $FLOW $TMPDIR/flow_${LK}.tif
 
-# rm $TMPDIR/mspa_${LK}.tif
-# cp $TMPDIR/mspa_${LK}_l.tif $TMPDIR/mspa_${LK}.tif
-# rm $TMPDIR/mspa_${LK}_l.tif
-# rm $GWB/input/lake_${LK}.tif
-
-
-# grass  -f --text --tmp-location $TMPDIR/overlapping_${LK}.tif <<EOF
-# r.external -o input=$TMPDIR/overlapping_${LK}.tif  output=out
-# r.external -o input=$TMPDIR/flow_${LK}.tif output=flow
-# r.neighbors input=flow output=flow_max method=maximum
-# r.neighbors input=flow output=flow_ave method=average
-#
-# r.mapcalc --o "flowInt = if(isnull(out), null() , flow)"
-# r.mapcalc --o "flowMax = if(isnull(out), null() , flow_max)"
-# r.mapcalc --o "flowAve = if(isnull(out), null() , flow_ave)"
-#
-# r.out.xyz --overwrite input=out output=$TMPDIR/coord_lake_${LK}.txt separator=space
-# r.out.xyz --overwrite input=flowInt output=$TMPDIR/coord_flowInt_${LK}.txt separator=space
-# r.out.xyz --overwrite input=flowMax output=$TMPDIR/coord_flowMax_${LK}.txt separator=space
-# r.out.xyz --overwrite input=flowAve output=$TMPDIR/coord_flowAve_${LK}.txt separator=space
-# EOF
 grass --tmp-project "$TMPDIR/overlapping_${LK}.tif" --exec bash -c "
     r.external -o input='$TMPDIR/overlapping_${LK}.tif' output=out
     r.external -o input='$TMPDIR/flow_${LK}.tif' output=flow
@@ -374,8 +264,6 @@ paste -d " " <(seq 1 $(wc -l < $TMPDIR/coord_lake_${LK}.txt))  \
 
 rm $TMPDIR/coord_lake_${LK}.txt $TMPDIR/coord_flowInt_${LK}.txt
 rm $TMPDIR/coord_flowMax_${LK}.txt $TMPDIR/coord_flowAve_${LK}.txt
-# rm $TMPDIR/overlapping_${LK}.tif
-# rm $GWB/input/mspa-parameters.txt
 
 # Identify streamIDs that have more than one duplicate
 DUP=$(awk '{print $4}' $TMPDIR/coord_lake_${LK}_p.txt | sort | uniq -c | awk '$1 > 1 {print $2}')
@@ -442,7 +330,6 @@ rm $TMPDIR/lbasin_reclass_${LK}.vrt
 rm $TMPDIR/coord_lake_${LK}*
 rm $TMPDIR/buffer_${LK}*
 rm $TMPDIR/flow_${LK}.tif
-# rm $TMPDIR/overlapping_${LK}.tif
 rm $TMPDIR/CompUnits_${LK}.vrt
 rm $TMPDIR/mspa_reclass_code.txt
 rm $TMPDIR/mspa_${LK}*
@@ -461,15 +348,6 @@ echo "Time taken: ${minutes} minutes and ${seconds} seconds"
 }
 
 export -f get_lake_intersection
-
-# for LK in "${LK[@]}"; do
-#   get_lake_intersection "$LK" &
-# done
-
-# wait
-
-# LK should be provided as an array from R
-# parallel -j $NCORES get_lake_intersection ::: "${LK[@]}"
 
 ids=$(awk -v id_name=${VAR_ID}  'NR == 1 { for (i=1; i<=NF; i++) {f[$i] = i} } \
     NR > 1 {print $(f[id_name])}' $DATA)
