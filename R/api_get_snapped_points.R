e5533e31d9044df83dd1bdedad5ddaf358c01a1d
afrogri37 <afrogri37@gmail.com>
2025-07-22 14:06:15 +0200

    output includes href

--- R/api_get_snapped_points.R
+++ R/api_get_snapped_points.R
@@ -1,7 +1,8 @@
 #' Snap Site Coordinates to the River Network
 #'
 #' Sends a request to the GeoFRESH API to snap a set of site coordinates to the nearest flowline
-#' in the river network. Returns both the snapped and original coordinates.
+#' in the river network. Returns both the snapped and original coordinates, along with a link
+#' to download the results or to use it in another function of the family ocgapi.
 #'
 #' @family ocgapi
 #' @param csv_url A URL to a CSV file hosted online. The CSV must contain latitude, longitude, and site ID columns.
@@ -11,8 +12,12 @@
 #' @param process_url URL of the pygeoapi process called by the function.
 #' #'   Must be specified explicitly, e.g.,
 #'   `"http://localhost:5000/processes/get-snapped-points/execution"`.
-
-#' @return A data frame containing the snapped coordinates and additional snapping information.
+#'
+#' @return A list with two elements:
+#' \describe{
+#'   \item{data}{A `data.frame` containing the snapped coordinates and additional information.}
+#'   \item{href}{A `character` string with the URL of the output CSV.}
+#' }
 #'
 #' @import httr
 #' @import jsonlite
@@ -27,14 +32,22 @@
 #'  colname_lon = "longitude",
 #'  colname_site_id = "site_id",
 #' process_url = "http://localhost:5000/processes/get-snapped-points/execution")
+#'
+#'
+#' #' head(result$data)
+#' cat("Download link:", result$href)
+#'
 #' }
+#'
+#'
+#'
 #'
 #' @export
 api_get_snapped_points <- function(csv_url,
-                               colname_lat = "latitude",
-                               colname_lon = "longitude",
-                               colname_site_id = "site_id",
-                               process_url = NULL) {
+                                   colname_lat = "latitude",
+                                   colname_lon = "longitude",
+                                   colname_site_id = "site_id",
+                                   process_url = NULL) {
 
   if (is.null(csv_url)) {
     stop("A 'csv_url' must be provided.")
@@ -44,7 +57,6 @@ api_get_snapped_points <- function(csv_url,
   if (is.null(process_url)) {
     stop("Please provide the URL of the pygeoapi process")
   }
-
 
   message("Using CSV-based API...")
 
@@ -57,7 +69,6 @@ api_get_snapped_points <- function(csv_url,
     ),
     outputs = list(transmissionMode = "reference")
   )
-
 
   response <- httr::POST(
     url = process_url,
@@ -83,5 +94,10 @@ api_get_snapped_points <- function(csv_url,
     stop("Error downloading CSV: ", e$message)
   })
 
-  return(df)
+  # Return both the data frame and the download link as a list
+  return(list(
+    data = df,
+    href = csv_download_url
+  ))
 }
+
