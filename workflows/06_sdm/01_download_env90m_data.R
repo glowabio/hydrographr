db1159961866592a77f592c0ddd073fb0994da2a
afrogri37 <afrogri37@gmail.com>
2026-05-05 13:22:46 +0200

    additionally download cti

--- workflows/06_sdm/01_download_env90m_data.R
+++ workflows/06_sdm/01_download_env90m_data.R
@@ -160,7 +160,7 @@ if (!is.na(landcover_file) && file.exists(landcover_file)) {
 message("\n--- Downloading Hydrography Variables ---")
 
 download_hydrography90m_tables(
-  subset = c("order_strahler", "length", "cum_length", "gradient", "elev_drop",
+  subset = c("cti", "order_strahler", "length", "cum_length", "gradient", "elev_drop",
              "accumulation", "channel_grad_dw_seg", "channel_grad_up_seg",
              "channel_elv_dw_seg", "channel_elv_up_seg", "connections",
              "stream_dist_dw_near", "stream_dist_up_near", "slope_grad_dw_cel"),
